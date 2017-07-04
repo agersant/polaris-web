8d925a1ef4a108bf1025a88650925a1c020a264c
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 14:35:24 -0700

    Fixed inaccurate test for detecting page

--- test/first_time_flow.js
+++ test/first_time_flow.js
@@ -2,18 +2,18 @@ module.exports = {
 	"Welcome page" : function(browser) {
 		browser.url("http://localhost:5050");
 		browser.expect.element("#initial-setup-page").to.be.present.before(1000);
-		browser.expect.element("initial-setup-welcome").to.be.present.before(1000);
-		browser.expect.element("initial-setup-welcome h2").text.to.contain("Welcome to Polaris");
+		browser.expect.element("#initial-setup-page").to.have.attribute("data-is").equals("initial-setup-welcome");
+		browser.expect.element("#initial-setup-page h2").text.to.contain("Welcome to Polaris");
 		browser.click("button.submit");
 	},
 
 	"Mount point setup" : function(browser) {
-		browser.expect.element("initial-setup-mount").to.be.present.before(1000);
-		browser.expect.element("initial-setup-mount h2").text.to.contain("Music Sources");
-		browser.expect.element('button.submit').to.not.be.enabled;
-		browser.setValue('input#source', 'test/collection');
-		browser.setValue('input#name', 'test_music');
-		browser.expect.element('button.submit').to.be.enabled;
+		browser.expect.element("#initial-setup-page").to.have.attribute("data-is").equals("initial-setup-mount");
+		browser.expect.element("#initial-setup-page h2").text.to.contain("Music Sources");
+		browser.expect.element("button.submit").to.not.be.enabled;
+		browser.setValue("input#source", "test/collection");
+		browser.setValue("input#name", "test_music");
+		browser.expect.element("button.submit").to.be.enabled;
 		browser.click("button.submit");
 	}
 };
