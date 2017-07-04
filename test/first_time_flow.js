80323eae616bbb6214ddcc02339b0ea2672f86c2
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 13:21:51 -0700

    Find initial flow container

--- test/first_time_flow.js
+++ test/first_time_flow.js
@@ -2,7 +2,8 @@ module.exports = {
   'Welcome page loads' : function (browser) {
     browser
       .url('http://localhost:5050')
-      .waitForElementVisible('body', 1000);
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('#initial-setup-page', 1000);
     browser.end();
   }
 };
