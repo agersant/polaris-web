ec6b851a902e10d4ec6f14d3fc4fcdf7eaa6a546
Antoine Gersant <antoine.gersant@lesforges.org>
2017-07-04 19:04:03 -0700

    More generous CI timeouts

--- test/first_time_flow.js
+++ test/first_time_flow.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"Welcome page" : function(browser) {
 		browser.url("http://localhost:5050");
-		browser.expect.element("#initial-setup-page").to.be.present.before(1000);
+		browser.expect.element("#initial-setup-page").to.be.present.before(2000);
 		browser.expect.element("#initial-setup-page").to.have.attribute("data-is").equals("initial-setup-welcome");
 		browser.expect.element("#initial-setup-page h2").text.to.contain("Welcome to Polaris");
 		browser.click("button.submit");
