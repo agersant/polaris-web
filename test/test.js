1a040f46fb0b78d3578ed2e3e263d9482ce86a45
Antoine Gersant <antoine.gersant@lesforges.org>
2018-10-08 22:07:56 -0700

    Nightmare.js setup

---
+++ test/test.js
@@ -0,0 +1,20 @@
+const Nightmare = require('nightmare')
+const assert = require('assert')
+
+describe('Load a Page', function() {
+  this.timeout('5s')
+
+  let nightmare = null
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  })
+
+  describe('/ (Home Page)', () => {
+    it('should load without error', done => {
+      nightmare.goto('http://localhost:5050')
+        .end()
+        .then(function (result) { done() })
+        .catch(done)
+    })
+  })
+})
