3e3a7a1a2142f2a907353eb966ab1c16f217c0a9
Antoine Gersant <antoine.gersant@lesforges.org>
2018-10-10 00:35:53 -0700

    Don't call expect within browser context

--- test/test.js
+++ test/test.js
@@ -2,9 +2,6 @@ const Nightmare = require('nightmare')
 const chai = require('chai')
 const expect = chai.expect
 
-console.log("CHAI " + chai)
-console.log("EXPECT " + expect)
-
 describe('First Time Flow', function() {
 
   let nightmare = new Nightmare()
@@ -19,10 +16,8 @@ describe('First Time Flow', function() {
   it('has a welcome message', done => {
     nightmare
       .wait('h2')
-      .evaluate(() => {
-        var title = document.querySelector('h2').innerHTML
-        expect(title).to.equal('Welcome to Polaris!')
-      })
+      .evaluate(() => document.querySelector('h2').innerHTML)
+      .then(title => { expect(title).to.equal('Welcome to Polaris!') })
       .click('button.submit')
       .then(() => { done() })
       .catch(done)
@@ -31,19 +26,14 @@ describe('First Time Flow', function() {
   it('can setup mount points', done => {
     nightmare
       .wait('button.submit')
-      .evaluate(() => {
-        var title = document.querySelector('h2').innerHTML
-        expect(title).to.equal('Music Sources')
-
-        var submitButton = document.querySelector('button.submit')
-        expect(submitButton.disabled.to.be.true)
-      })
+      .evaluate(() => document.querySelector('h2').innerHTML)
+      .then(title => { expect(title).to.equal('Music Sources') })
+      .evaluate(() => document.querySelector('button.submit').disabled)
+      .then(buttonDisabled => { expect(buttonDisabled).to.be.true })
       .type('input#source', 'test/collection')
       .type('input#name', 'test_music')
-      .evaluate(() => {
-        var submitButton = document.querySelector('button.submit')
-        expect(submitButton.disabled.to.not.be.true)
-      })
+      .evaluate(() => document.querySelector('button.submit').disabled)
+      .then(buttonDisabled => { expect(buttonDisabled).to.not.be.true })
       .click('button.submit')
       .then(() => { done() })
       .catch(done)
@@ -51,20 +41,15 @@ describe('First Time Flow', function() {
 
   it('can setup first user', done => {
     nightmare
-      .evaluate(() => {
-        var title = document.querySelector('h2').innerHTML
-        expect(title).to.equal('User Account')
-
-        var submitButton = document.querySelector('button.submit')
-        expect(submitButton.disabled.to.be.true)
-      })
+      .evaluate(() => document.querySelector('h2').innerHTML)
+      .then(title => { expect(title).to.equal('User Account') })
+      .evaluate(() => document.querySelector('button.submit').disabled)
+      .then(buttonDisabled => { expect(buttonDisabled).to.be.true })
       .type('input#username', 'agersant')
       .type('input#password', 'very_secret')
       .type('input#password_confirm', 'very_secret')
-      .evaluate(() => {
-        var submitButton = document.querySelector('button.submit')
-        expect(submitButton.disabled.to.not.be.true)
-      })
+      .evaluate(() => document.querySelector('button.submit').disabled)
+      .then(buttonDisabled => { expect(buttonDisabled).to.not.be.true })
       .click('button.submit')
       .then(() => { done() })
       .catch(done)
