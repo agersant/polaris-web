8c7aa568bccc08cdb6e462a1f2c03fe6838072ba
Antoine Gersant <antoine.gersant@lesforges.org>
2018-10-10 01:12:21 -0700

    Split tests

--- test/test.js
+++ test/test.js
@@ -18,66 +18,75 @@ describe('First Time Flow', function() {
       .wait('h2')
       .evaluate(() => document.querySelector('h2').innerHTML)
       .then(title => { expect(title).to.equal('Welcome to Polaris!') })
-      .catch(done)
-
-    nightmare
-      .click('button.submit')
       .then(() => { done() })
       .catch(done)
   })
 
-  it('can setup mount points', done => {
+  it('has a submit button', done => {
     nightmare
       .wait('button.submit')
+      .then(() => { done() })
+      .catch(done)
+  })
+
+  it('transitions to mount point setup', done => {
+    nightmare
+      .click('button.submit')
       .evaluate(() => document.querySelector('h2').innerHTML)
       .then(title => { expect(title).to.equal('Music Sources') })
+      .then(() => { done() })
       .catch(done)
+  })
 
+  it('cannot submit prematurely', done => {
     nightmare
       .evaluate(() => document.querySelector('button.submit').disabled)
       .then(buttonDisabled => { expect(buttonDisabled).to.be.true })
+      .then(() => { done() })
       .catch(done)
+  })
 
+  it('can fill out the mount point form', done => {
     nightmare
       .type('input#source', 'test/collection')
       .type('input#name', 'test_music')
       .evaluate(() => document.querySelector('button.submit').disabled)
       .then(buttonDisabled => { expect(buttonDisabled).to.not.be.true })
-      .catch(done)
-
-    nightmare
-      .click('button.submit')
       .then(() => { done() })
       .catch(done)
   })
 
-  it('can setup first user', done => {
+  it('transitions to user setup', done => {
     nightmare
+      .click('button.submit')
       .evaluate(() => document.querySelector('h2').innerHTML)
       .then(title => { expect(title).to.equal('User Account') })
+      .then(() => { done() })
       .catch(done)
+  })
 
+  it('cannot submit prematurely', done => {
     nightmare
       .evaluate(() => document.querySelector('button.submit').disabled)
       .then(buttonDisabled => { expect(buttonDisabled).to.be.true })
+      .then(() => { done() })
       .catch(done)
+  })
 
+  it('can fill out the user account form', done => {
     nightmare
       .type('input#username', 'agersant')
       .type('input#password', 'very_secret')
       .type('input#password_confirm', 'very_secret')
       .evaluate(() => document.querySelector('button.submit').disabled)
       .then(buttonDisabled => { expect(buttonDisabled).to.not.be.true })
-      .catch(done)
-
-    nightmare
-      .click('button.submit')
       .then(() => { done() })
       .catch(done)
   })
 
   it('transitions to main page', done => {
     nightmare
+      .click('button.submit')
       .wait('main menu')
       .end()
       .then(() => { done() })
