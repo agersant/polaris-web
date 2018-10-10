6c4f4f279227d9cd48ac79f16d444a8974769ba0
Antoine Gersant <antoine.gersant@lesforges.org>
2018-10-09 23:55:20 -0700

    Added first time flow tests

--- test/test.js
+++ test/test.js
@@ -6,20 +6,65 @@ describe('First Time Flow', function() {
 
   let nightmare = new Nightmare()
 
-  it('should load without error', done => {
+  it('loads without error', done => {
     nightmare.goto('http://localhost:5050')
       .then(result => { done() })
       .catch(done)
   })
 
-  it('should have a welcome message', done => {
-    nightmare.wait('#initial-setup-page')
-      .evaluate(() => document.querySelector('#initial-setup-page h2').innerHTML)
-      .end()
-      .then(title => {
-        expect(title).to.equal('Welcome to Polaris!')
-        done()
-      })
-      .catch(done)
+  it('has a welcome message', done => {
+    nightmare.evaluate(() => {
+      var title = document.querySelector('h2').innerHTML
+      expect(title).to.equal('Welcome to Polaris!')
+    })
+    .click('button.submit')
+    .then(() => { done() })
+    .catch(done)
+  })
+
+  it('can setup mount points', done => {
+    nightmare.evaluate(() => {
+      var title = document.querySelector('h2').innerHTML
+      expect(title).to.equal('Music Sources')
+
+      var submitButton = document.querySelector('button.submit')
+      expect(submitButton.disabled.to.be.true)
+    })
+    .type('input#source', 'test/collection')
+    .type('input#name', 'test_music')
+    .evaluate(() => {
+      var submitButton = document.querySelector('button.submit')
+      expect(submitButton.disabled.to.not.be.true)
+    })
+    .click('button.submit')
+    .then(() => { done() })
+    .catch(done)
+  })
+
+  it('can setup first user', done => {
+    nightmare.evaluate(() => {
+      var title = document.querySelector('h2').innerHTML
+      expect(title).to.equal('User Account')
+
+      var submitButton = document.querySelector('button.submit')
+      expect(submitButton.disabled.to.be.true)
+    })
+    .type('input#username', 'agersant')
+    .type('input#password', 'very_secret')
+    .type('input#password_confirm', 'very_secret')
+    .evaluate(() => {
+      var submitButton = document.querySelector('button.submit')
+      expect(submitButton.disabled.to.not.be.true)
+    })
+    .click('button.submit')
+    .then(() => { done() })
+    .catch(done)
+  })
+
+  it('transitions to main page', done => {
+    nightmare.wait('main menu')
+    .end()
+    .then(() => { done() })
+    .catch(done)
   })
 })
