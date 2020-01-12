37fb985c43e05176382e5825ad7161c08f38377b
Antoine Gersant <antoine.gersant@lesforges.org>
2020-01-12 01:16:43 -0800

    More cypress setup

--- tests/initial-setup.js
+++ tests/initial-setup.js
@@ -1,5 +1,25 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
+describe('Initial Setup', function() {
+  it('can be completed', function() {
+    cy.visit('/')
+
+    cy.get('h2').should('eq', 'Welcome to Polaris!')
+    cy.get('button.submit').click()
+
+    cy.get('h2').should('eq', 'Mount Points')
+    cy.get('button.submit').should('be.disabled')
+    cy.get('input#source').type('test/collection')
+    cy.get('input#name').type('test_music')
+    cy.get('button.submit').should('not.be.disabled')
+    cy.get('button.submit').click()
+
+    cy.get('h2').should('eq', 'User Account')
+    cy.get('button.submit').should('be.disabled')
+    cy.get('input#username').type('testUser')
+    cy.get('input#password').type('testPassword')
+    cy.get('input#password_confirm').type('testPassword')
+    cy.get('button.submit').should('not.be.disabled')
+    cy.get('button.submit').click()
+
+    cy.get('main menu').should('exist')
   })
 })
