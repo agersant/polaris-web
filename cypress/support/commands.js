6ff5b54c939acdd0b2eb8272b7b66a6c84991d50
Antoine Gersant <antoine.gersant@lesforges.org>
2020-01-12 15:38:55 -0800

    Test login form

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,42 @@
-Cypress.Commands.add("wipeInitialSetup", () => {
-	cy.request({
-		method: 'POST',
-		url: '/api/auth',
-		body: {
-			username: "testUser",
-			password: "testPassword"
-		},
-		failOnStatusCode: false
-	}).then((auth) => {
-		if (auth.status == 200) {
-			cy.request('PUT', '/api/settings', {
-				users: [],
-				mount_dirs: []
-			})
-		}
+Cypress.Commands.add('wipeInitialSetup', () => {
+
+	let wipe = () => {
+		cy.request('PUT', '/api/settings', {
+			users: [],
+			mount_dirs: []
+		})
 		cy.clearCookies()
-	});
+	}
+
+	cy.request('GET', '/api/initial_setup')
+		.then((r) => {
+			if (!r.body.has_any_users) {
+				wipe()
+			} else {
+				cy.request('POST', '/api/auth', {
+					username: 'testUser',
+					password: 'testPassword'
+				}).then(wipe)
+			}
+		})
+})
+
+Cypress.Commands.add('completeInitialSetup', () => {
+	cy.request('GET', '/api/initial_setup')
+		.then((r) => {
+			if (r.body.has_any_users) {
+				return
+			}
+			cy.request('PUT', '/api/settings', {
+				users: [{
+					username: 'testUser',
+					password: 'testPassword'
+				}],
+				mount_dirs: [{
+					source: 'tests/collection',
+					name: 'Test'
+				}]
+			})
+		})
+	cy.clearCookies()
 })
--- tags/auth.tag
+++ tags/auth.tag
@@ -3,9 +3,9 @@
 		<div class="content">
 			<img class="logo" src="img/logo.png"/>
 			<form name="authForm" onsubmit={ doLogin }>
-				<input type="text" name="username" placeholder="Username" autofocus/>
-				<input type="password" name="password" placeholder="Password"/>
-				<p if={ badCredentials } class="tip error">Incorrect credentials, please try again.</p>
+				<input data-cy="username" type="text" name="username" placeholder="Username" autofocus/>
+				<input data-cy="password" type="password" name="password" placeholder="Password"/>
+				<p if={ badCredentials } data-cy="login-error" class="tip error">Incorrect credentials, please try again.</p>
 				<input type="submit" value="Login"/>
 			</form>
 		</div>
