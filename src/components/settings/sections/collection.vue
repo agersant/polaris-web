218d31a0c96a78853e6ba9f69d4d528228872add
Antoine Gersant <antoine.gersant@lesforges.org>
2020-01-25 18:30:19 -0800

    ported collection settings

--- src/components/settings/sections/collection.vue
+++ src/components/settings/sections/collection.vue
@@ -1,10 +1,16 @@
 <template>
-	<p>COLLECTION</p>
-	<!-- <form if={ config } onsubmit={ save }>
+	<form v-if="settings">
 		<div class="field">
-			<label for="art_pattern">Album art pattern</label><input type="text" id="art_pattern" value={ config.album_art_pattern } oninput={ onPatternInput } placeholder="Folder.(jpg|png)"/>
+			<label for="art_pattern">Album art pattern</label>
+			<input
+				type="text"
+				id="art_pattern"
+				v-model="settings.album_art_pattern"
+				v-on:change="onAlbumArtPatternChanged"
+				placeholder="Folder.(jpg|png)"
+			/>
 			<p class="tip">The regular expression used to detect album art files.</p>
-			<p if={ !isPatternValid( config.album_art_pattern ) } class="tip error">Please enter a valid regular expression.</p>
+			<p v-if="!validateAlbumArtPattern()" class="tip error">Please enter a valid regular expression.</p>
 		</div>
 		<div class="field sources">
 			<label>Music sources</label>
@@ -12,119 +18,134 @@
 				<thead>
 					<th>Location</th>
 					<th>Name</th>
-					<th/>
+					<th />
 				</thead>
-				<tr each={ mountPoint in mountPoints }>
-					<td><input type="text" value={ mountPoint.source } oninput={ onPathInput } /></td>
-					<td><input type="text" value={ mountPoint.name } oninput={ onNameInput }/></td>
-					<td><i onClick={ deleteMountPoint } class="noselect material-icons md-18">delete</i></td>
+				<tr v-for="(mountPoint, index) in settings.mount_dirs" v-bind:key="index">
+					<td>
+						<input type="text" v-model="mountPoint.source" v-on:change="commit" />
+					</td>
+					<td>
+						<input type="text" v-model="mountPoint.name" v-on:change="commit" />
+					</td>
+					<td>
+						<i v-on:click="deleteMountPoint(index)" class="noselect material-icons md-18">delete</i>
+					</td>
 				</tr>
 			</table>
-			<button onClick={ addSource }>Add more</button>
+			<button v-on:click="addMountPoint">Add more</button>
 		</div>
 		<div class="field sleep_duration">
-			<label for="sleep_duration">Scan collection every
-				<input type="text" id="sleep_duration" value={ Math.round(config.reindex_every_n_seconds / 60) } oninput={ onSleepInput } placeholder=""/> minutes
+			<label for="sleep_duration">
+				Scan collection every
+				<input
+					type="text"
+					id="sleep_duration"
+					v-model="reindexPeriod"
+					v-on:change="onReindexPeriodChanged"
+				/> minutes
 			</label>
-			<async-button ref="reindex" submit={ false } states={ reindexStates } action={ reindex }/>
+			<state-button
+				ref="reindex"
+				v-bind:submit="false"
+				v-bind:states="reindexStates"
+				v-bind:state="reindexState"
+				v-on:click="reindex"
+			/>
 		</div>
-		<settings-apply disabled={ !isPatternValid(config.album_art_pattern) } />
-	</form>-->
+	</form>
 </template>
 
 <script>
-// var self = this;
-// this.config = null;
-// this.mountPoints = null;
+import * as Utils from "/src/utils";
+import StateButton from "/src/components/state-button";
+export default {
+	components: {
+		"state-button": StateButton
+	},
 
-// this.on('mount', function() {
-// 	utils.api("/settings")
-// 	.then(function(res) { return res.json(); })
-// 	.then(function(data) {
-// 		this.config = data;
-// 		this.mountPoints = data.mount_dirs;
-// 		this.update();
-// 	}.bind(self));
-// });
+	data() {
+		return {
+			settings: null,
+			reindexPeriod: 0,
+			reindexStates: {
+				ready: { name: "Scan now" },
+				applying: { name: "Hold on…", disabled: true },
+				success: { name: "On it!", disabled: true, success: true },
+				failure: { name: "Error :(", disabled: true, failure: true }
+			},
+			reindexState: null
+		};
+	},
 
-// isPatternValid(pattern) {
-// 	try {
-// 		var re = new RegExp(pattern);
-// 		return true;
-// 	} catch(e) {
-// 		return false;
-// 	}
-// }
+	mounted() {
+		this.reindexState = this.reindexStates.ready;
+		Utils.api("/settings")
+			.then(res => res.json())
+			.then(data => {
+				this.settings = data;
+				this.reindexPeriod = Math.round(data.reindex_every_n_seconds / 60);
+			});
+	},
 
-// onPatternInput(e) {
-// 	this.config.album_art_pattern = e.target.value;
-// }
+	methods: {
+		onAlbumArtPatternChanged() {
+			if (this.validateAlbumArtPattern()) {
+				this.commit();
+			}
+		},
 
-// onSleepInput(e) {
-// 	var newDuration = Math.round(e.target.value) * 60;
-// 	if (isNaN(newDuration) || newDuration < 0) {
-// 		newDuration = 0;
-// 	}
-// 	e.target.value = newDuration / 60;
-// 	this.config.reindex_every_n_seconds = newDuration * 1;
-// }
+		validateAlbumArtPattern() {
+			try {
+				new RegExp(this.settings.album_art_pattern);
+				return true;
+			} catch (e) {
+				return false;
+			}
+		},
 
-// onPathInput(e) {
-// 	e.item.mountPoint.source = e.target.value;
-// }
+		onReindexPeriodChanged() {
+			let periodInSeconds = Math.round(this.reindexPeriod) * 60;
+			if (isNaN(periodInSeconds) || periodInSeconds < 0) {
+				return;
+			}
+			this.settings.reindex_every_n_seconds = periodInSeconds;
+			this.commit();
+		},
 
-// onNameInput(e) {
-// 	e.item.mountPoint.name = e.target.value;
-// }
+		addMountPoint() {
+			this.settings.mount_dirs.push({ name: "", source: "" });
+		},
 
-// addSource(e) {
-// 	e.preventDefault();
-// 	this.mountPoints.push({});
-// }
+		deleteMountPoint(index) {
+			this.settings.mount_dirs.splice(index, 1);
+		},
 
-// deleteMountPoint(e) {
-// 	e.stopPropagation();
-// 	if (this.mountPoints.length == 1) {
-// 		this.mountPoints = [{}];
-// 	} else {
-// 		var mountPointIndex = this.mountPoints.indexOf(e.item.mountPoint);
-// 		if (mountPointIndex >= 0) {
-// 			this.mountPoints.splice(mountPointIndex, 1);
-// 		}
-// 	}
-// }
+		reindex() {
+			this.reindexState = this.reindexStates.applying;
+			Utils.api("/trigger_index", { method: "POST" }).then(res => {
+				if (res.status != 200) {
+					this.reindexState = this.reindexStates.failure;
+					console.log("Index trigger error: " + res.status);
+				} else {
+					this.reindexState = this.reindexStates.success;
+				}
+				setTimeout(() => {
+					this.reindexState = this.reindexStates.ready;
+				}, 2000);
+			});
+		},
 
-// this.reindexStates = {
-// 	ready: { name: "Scan now", init: true },
-// 	applying: { name: "Hold on…", disabled: true },
-// 	success: { name: "On it!", disabled: true, success: true },
-// 	failure: { name: "Error :(", disabled: true, failure: true },
-// };
-
-// reindex(e) {
-// 	e.preventDefault();
-// 	this.refs.reindex.setState(this.reindexStates.applying);
-// 	utils.api("/trigger_index", { method: "POST" })
-// 	.then(function(res) {
-// 		if (res.status != 200) {
-// 			this.refs.reindex.setState(this.reindexStates.failure);
-// 			console.log("Index trigger error: " + res.status);
-// 		} else {
-// 			this.refs.reindex.setState(this.reindexStates.success);
-// 		}
-// 		setTimeout(function() {
-// 			if (!this.refs || !this.refs.reindex) {
-// 				return;
-// 			}
-// 			this.refs.reindex.setState(this.reindexStates.ready);
-// 		}.bind(this), 2000);
-// 	}.bind(this));
-// }
-
-// save(e) {
-// 	e.preventDefault();
-// 	eventBus.trigger("settings:submitConfig", this.config);
-// }
+		commit() {
+			Utils.api("/settings", {
+				method: "PUT",
+				body: JSON.stringify(this.settings),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+		}
+	}
+};
 </script>
 
 <style scoped>
@@ -133,7 +154,7 @@
 }
 
 .sleep_duration input {
-	display: inline;
+	width: 50px;
 	text-align: right;
 }
 </style>
---
+++ src/components/state-button.vue
@@ -0,0 +1,142 @@
+<template>
+	<button
+		v-on:click="onClick"
+		v-bind:disabled="disabled || state.disabled"
+		v-bind:type="submit ? 'submit' : 'button'"
+		v-bind:class="{ submit: submit, success: state.success, failure: state.failure }"
+	>
+		<div class="status">
+			<span
+				v-for="(possibleState, index) in states"
+				v-bind:key="index"
+				v-bind:class="{
+					'tick-in': possibleState == state && canAnimate,
+					'tick-out': possibleState == previousState && canAnimate,
+					'snap-out': possibleState != state && possibleState != previousState
+				}"
+			>{{ possibleState.name }}</span>
+		</div>
+	</button>
+</template>
+
+<script>
+export default {
+	props: {
+		submit: {
+			type: Boolean,
+			required: true
+		},
+		states: {
+			type: Object,
+			required: true
+		},
+		state: {
+			type: Object,
+			default: function() {
+				return {};
+			}
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	},
+
+	data() {
+		return {
+			canAnimate: false,
+			previousState: null
+		};
+	},
+
+	watch: {
+		state: function(to, from) {
+			this.previousState = from;
+			this.canAnimate = from != null;
+		}
+	},
+
+	methods: {
+		onClick() {
+			this.$emit("click");
+		}
+	}
+};
+</script>
+
+<style scoped>
+button {
+	width: 95px;
+	height: 32px;
+	padding-left: 0;
+	padding-right: 0;
+}
+
+button.success {
+	transition: all 250ms ease-in-out;
+	background-color: var(--theme-good);
+	border: 0;
+}
+
+button.failure {
+	transition: all 250ms ease-in-out;
+	background-color: var(--theme-bad);
+	border: 0;
+}
+
+button.success * {
+	color: var(--theme-foreground-against-good);
+}
+
+button.failure * {
+	color: var(--theme-foreground-against-bad);
+}
+
+.status {
+	text-align: center;
+	height: 100%;
+	width: 100%;
+	position: relative;
+	overflow: hidden;
+}
+
+.status span {
+	position: absolute;
+	left: 0;
+	right: 0;
+	margin: auto;
+}
+
+.tick-in {
+	animation: in 250ms ease-in-out;
+	animation-fill-mode: forwards;
+}
+
+.tick-out {
+	animation: out 250ms ease-in-out;
+	animation-fill-mode: forwards;
+}
+
+.snap-out {
+	animation: out 0ms;
+	animation-fill-mode: forwards;
+}
+
+@keyframes in {
+	from {
+		top: 40px;
+	}
+	to {
+		top: 0px;
+	}
+}
+
+@keyframes out {
+	from {
+		top: 0px;
+	}
+	to {
+		top: -40px;
+	}
+}
+</style>
