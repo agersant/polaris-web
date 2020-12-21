4dca85fe7357998ef54e9a5ac95851cd1463410a
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-20 19:52:36 -0800

    Workaround for test flakiness

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -91,6 +91,7 @@ Cypress.Commands.add('login', () => {
 		username: 'testUser',
 		password: 'testPassword'
 	}).then((resp) => {
+		window.localStorage.clear() // TODO: This is a workaround for https://github.com/cypress-io/cypress/issues/2695
 		window.localStorage.setItem('username', resp.body.username)
 		window.localStorage.setItem('authToken', resp.body.token)
 		window.localStorage.setItem('isAdmin', resp.body.is_admin)
