047de4fa97e46685642966acfc4d6fddf741261d
Antoine Gersant <antoine.gersant@lesforges.org>
2022-03-21 19:58:31 -0700

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue"
 import router from "./router"
 import store from "./store/store"
-import EntryPoint from "./components/entry-point"
+import EntryPoint from "./components/entry-point.vue"
 
 store.dispatch("initialSetup/refresh").then(() => {
 	const app = createApp(EntryPoint);
