837de5a744399d30f44441184c9ec6ecab030979
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-11 17:07:55 -0700

    Implements sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,114 +1,76 @@
 <template>
-	<div data-cy="sidebar">
-		<ul class="main-nav">
-			<li v-for="button in buttons" v-bind:key="button.url" v-on:click="onClickButton(button)"
-				v-bind:data-cy="button.cy" v-bind:class="{ noselect: 1, selected: button.pattern.test(currentURL) }">
-				<i class="noselect material-icons md-18">{{ button.icon }}</i>
-			</li>
-		</ul>
-		<ul>
-			<li v-on:click="user.logout" v-bind:class="{ noselect: 1 }" data-cy="logout">
-				<i class="noselect material-icons md-18">exit_to_app</i>
-			</li>
-		</ul>
-	</div>
+	<Menu :model="menuItems" class="w-80 p-6 overflow-hidden" pt:submenuLabel:class="text-sm uppercase"
+		pt:separator:class="my-2 opacity-0">
+		<template #start>
+			<img src="/assets/logo.svg" class="my-8 mx-4" />
+		</template>
+		<template #item="{ item }">
+			<a :class="['p-menu-item-link', ...itemClasses(item)]" :data-cy="item['data-cy']" tabindex="-1">
+				<span :class="['material-icons-round', ...iconClasses(item)]">{{ item.icon }}</span>
+				<span class="p-menu-item-label">{{ item.label }}</span>
+			</a>
+		</template>
+	</Menu>
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from "@/stores/user";
-import { ref, watch } from "vue";
+import Menu from 'primevue/menu';
+import { MenuItem } from 'primevue/menuitem';
+import { computed, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+
+import { useUserStore } from "@/stores/user";
 
 const route = useRoute();
 const router = useRouter();
 const user = useUserStore();
 
-type SidebarButton = {
-	cy: string,
-	icon: string,
-	url: string,
-	pattern: RegExp,
+const currentURL = computed(() => route.path);
+
+const navigateTo = (url: string) => { return () => router.push(url).catch(err => { }); };
+
+const itemClasses = (item: MenuItem) => {
+	if (!item.pattern || !item.pattern.test(currentURL.value)) {
+		return [];
+	}
+	return ["bg-primary", "rounded-md", "!text-primary-contrast", "font-medium"];
 }
 
-const currentURL = ref("");
-const buttons: SidebarButton[] = [
-	{
-		cy: "browse",
-		icon: "library_music",
-		url: "/browse",
-		pattern: new RegExp("(browse|^/$)"),
-	},
-	{
-		cy: "random",
-		icon: "shuffle",
-		url: "/random",
-		pattern: new RegExp("random"),
-	},
-	{
-		cy: "recent",
-		icon: "new_releases",
-		url: "/recent",
-		pattern: new RegExp("recent"),
-	},
-	{
-		cy: "playlists",
-		icon: "playlist_play",
-		url: "/playlists",
-		pattern: new RegExp("playlist"),
-	},
-	{
-		cy: "search",
-		icon: "search",
-		url: "/search",
-		pattern: new RegExp("search"),
-	},
-	{
-		cy: "settings",
-		icon: "settings",
-		url: "/settings/preferences",
-		pattern: new RegExp("settings"),
-	},
-];
-
-watch(() => route.path,
-	(path) => {
-		currentURL.value = path || buttons[0].url;
-	},
-	{ immediate: true }
-);
-
-function onClickButton(button: SidebarButton) {
-	router.push(button.url).catch(err => {});
+const iconClasses = (item: MenuItem) => {
+	if (!item.pattern || !item.pattern.test(currentURL.value)) {
+		return ["p-menu-item-icon"];
+	}
+	return [];
 }
+
+const menuItems = ref([
+	{
+		label: "Library",
+		items: [
+			{ label: "Files", icon: "folder", "data-cy": "files", pattern: new RegExp("files"), command: navigateTo("/files") },
+			{ label: "Artists", icon: "person", "data-cy": "artists", pattern: new RegExp("artists"), command: navigateTo("/artists") },
+			{ label: "Albums", icon: "library_music", "data-cy": "albums", pattern: new RegExp("albums"), command: navigateTo("/albums") },
+			{ label: "Songs", icon: "music_note", "data-cy": "songs", pattern: new RegExp("songs"), command: navigateTo("/songs") },
+			{ label: "Search", icon: "search", "data-cy": "search", pattern: new RegExp("search"), command: navigateTo("/search") },
+		]
+	},
+	{ separator: true },
+	{
+		label: "Profile",
+		items: [
+			{ label: "Playlists", icon: "playlist_play", "data-cy": "playlists", pattern: new RegExp("playlist"), command: navigateTo("/playlists") },
+			{ label: "Settings", icon: "settings", "data-cy": "settings", pattern: new RegExp("settings"), command: navigateTo("/settings/preferences") },
+			{ label: "Sign Out", icon: "logout", "data-cy": "logout", command: () => { user.logout(); } },
+		]
+	},
+	{ separator: true },
+	{
+		label: "Playlists",
+		items: [
+			{ label: "Summer", icon: "queue_music" },
+			{ label: "Fall", icon: "queue_music" },
+			{ label: "Winter", icon: "queue_music" },
+		]
+	},
+]);
 </script>
-
-<style scoped>
-div {
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-}
-
-ul {
-	text-align: center;
-	background-color: var(--theme-menu-background);
-}
-
-ul.main-nav {
-	flex-grow: 1;
-}
-
-li {
-	height: 50px;
-	box-sizing: border-box;
-}
-
-li i.material-icons {
-	color: var(--theme-foreground-against-accent);
-	line-height: 50px;
-}
-
-.selected {
-	background-color: var(--theme-accent);
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Aura from '@primevue/themes/aura';
 import { createPinia } from "pinia";
 import PrimeVue from 'primevue/config';
+import { definePreset } from '@primevue/themes';
 import { createApp } from "vue";
 
 import EntryPoint from "@/components/EntryPoint.vue";
@@ -10,10 +11,16 @@ import router from "./router";
 import "material-icons/iconfont/material-icons.css";
 import "./style.css";
 
+const MyPreset = definePreset(Aura, {
+    components: {
+        menu: { submenuLabel: { fontWeight: 500, } },
+    },
+});
+
 createApp(EntryPoint)
     .use(createPinia())
     .use(router)
-    .use(PrimeVue, { theme: { preset: Aura } })
+    .use(PrimeVue, { theme: { preset: MyPreset } })
     .mount("#vue-container");
 
 usePreferencesStore();
