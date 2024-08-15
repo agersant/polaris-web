d6cebc89fcb6ce35db33b3b775fcc3b654e535f6
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-14 18:56:31 -0700

    Virtual tree poc

---
+++ src/components/basic/VirtualTree.vue
@@ -0,0 +1,48 @@
+<template>
+    <VirtualScroller :items="props.value" :itemSize="50">
+        <template v-slot:item="{ item, options }">
+            <VirtualTreeNode style="height: 50px" :node="item" @node-toggle="onNodeToggle"
+                :expanded="expandedKeys.has(item.key)" />
+        </template>
+    </VirtualScroller>
+</template>
+
+<script setup lang="ts">
+import VirtualScroller from 'primevue/virtualscroller';
+import { ref } from 'vue';
+
+import VirtualTreeNode from "./VirtualTreeNode.vue";
+
+export interface Node {
+    depth: number,
+    key: string,
+    label: string,
+    icon?: string,
+    leaf: boolean,
+    loading?: boolean,
+}
+
+const props = defineProps<{
+    value: Node[],
+}>();
+
+const emit = defineEmits<{
+    (e: 'node-collapse', node: Node): void,
+    (e: 'node-expand', node: Node): void,
+}>();
+
+const expandedKeys = ref(new Set<string>());
+
+function onNodeToggle(node: Node) {
+    const key = node.key;
+
+    if (expandedKeys.value.has(key)) {
+        expandedKeys.value.delete(key);
+        emit('node-collapse', node);
+    } else {
+        expandedKeys.value.add(key);
+        emit('node-expand', node);
+    }
+}
+
+</script>
