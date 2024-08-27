73170a359ac4b8059ca898adbbb9905dfbdfe8d4
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-26 21:29:10 -0700

    Playlist style fine-tuning

--- src/components/basic/SectionTitle.vue
+++ src/components/basic/SectionTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mt-11 pb-3 border-b border-ls-200 flex items-center justify-between">
-        <h3 class="text-4xl tracking-wider text-ls-500">{{ label }}</h3>
+        <h3 class="text-4xl tracking-wider text-ls-500 select-none">{{ label }}</h3>
         <slot name="right" />
     </div>
 </template>
--- src/components/playback/PlaylistSong.vue
+++ src/components/playback/PlaylistSong.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="px-4 flex whitespace-nowrap items-center text-xs" :class="rowClass" :style="{ height: height + 'px' }">
+    <div class="flex whitespace-nowrap items-center rounded-sm text-xs" :class="rowClass"
+        :style="{ height: height + 'px' }">
         <!-- TODO fallback when metadata not loaded -->
         <!-- TODO tooltips -->
         <!-- TODO removal support -->
@@ -7,7 +8,7 @@
         <!-- TODO placeholder while image is loading or failed to load -->
         <img v-if="!compact && thumbnailURL" class="basis-10 mr-3 shrink-0 h-10 rounded-md" :src="thumbnailURL" />
         <div class="grow basis-0 pr-4 overflow-hidden text-ellipsis" v-if="song">{{ formatTrackContext(song) }}</div>
-        <div class="basis-8 shrink-0 text-right mr-2" v-if="song">{{ formatTrackNumber(song) }}</div>
+        <div class="basis-8 shrink-0 text-right mr-1" v-if="song">{{ formatTrackNumber(song) }}.</div>
         <div class="grow basis-0 pr-4 overflow-hidden text-ellipsis" v-if="song">
             {{ formatTitle(song) }}
             <span :class="selected ? '' : 'text-ls-400'"
@@ -16,8 +17,8 @@
             </span>
         </div>
         <div class="basis-16 shrink-0 text-right" v-if="song">{{ formatTrackDuration(song) }}</div>
-        <div v-if="focused" class="absolute h-full outline-1 outline-dotted -outline-offset-4 outline-accent-500"
-            :class="compact ? 'left-1 right-1 rounded-md' : 'left-1.5 right-1.5 rounded-lg'" />
+        <div v-if="focused"
+            class="absolute left-px right-px top-px bottom-px outline-1 outline-dotted outline-accent-500" />
     </div>
 </template>
 
@@ -52,11 +53,14 @@ const rowClass = computed(() => {
     if (props.selected) {
         background = "bg-accent-100";
     } else if (props.index % 2 == 1) {
-        background = "bg-ls-50";
+        background = props.compact ? "bg-ls-50" : "bg-gradient-to-r from-ls-0 to-ls-50 to-[50px]";
     }
 
     return [
         background,
+        props.compact ? "px-3" : "px-2",
+        !props.compact && !props.selected ? "mr-2" : "",
+        !props.compact && props.selected ? "pr-4" : "",
         props.selected ? "text-accent-700 dark:text-accent-200" : "text-ls-700 dark:text-ds-400",
     ];
 });
@@ -73,16 +77,6 @@ function formatTrackContext(song: Song) {
     context += " - ";
     context += song.album ? song.album : "Unknown Album";
     return context;
-}
-
-function formatTrackDetails(song: Song) {
-    let details = "";
-    if (song.track_number) {
-        details += song.track_number;
-        details += ". ";
-    }
-    details += formatTitle(song);
-    return details;
 }
 
 function formatTrackDuration(song: Song) {
