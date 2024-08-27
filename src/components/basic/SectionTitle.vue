aa9ee5ebe6a073d77c26855d7e6bc13c4af8f4c9
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-26 20:44:00 -0700

    Adds reusable section title

---
+++ src/components/basic/SectionTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="mt-11 pb-3 border-b border-ls-200 flex items-center justify-between">
+        <h3 class="text-4xl tracking-wider text-ls-500">{{ label }}</h3>
+        <slot name="right" />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{ label: string }>();
+
+</script>
