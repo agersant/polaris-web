d689befaf0a428ecd066a574d694239e9db0980a
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-29 22:28:32 -0700

    Narrow window support

--- src/components/basic/SectionTitle.vue
+++ src/components/basic/SectionTitle.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="mt-11 mb-8 pb-3 border-b border-ls-200 dark:border-ds-700 flex items-center justify-between">
-        <h3 class="text-4xl tracking-wider text-ls-500 dark:text-ds-400 select-none">{{ label }}</h3>
+        <h3
+            class="min-w-0 mr-8 text-4xl tracking-wider text-ls-500 dark:text-ds-400 select-none whitespace-nowrap overflow-hidden text-ellipsis">
+            {{ label }}
+        </h3>
         <slot name="right" />
     </div>
 </template>
