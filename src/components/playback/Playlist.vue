9d59e69e9337fcda0fb195d896ac6a757d24d7c0
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-30 20:48:29 -0700

    Whitespace adjustment

--- src/components/playback/Playlist.vue
+++ src/components/playback/Playlist.vue
@@ -1,6 +1,7 @@
 <template>
 
-	<div class="flex flex-col py-8 px-16 border-l border-ls-200 dark:border-ds-700 select-none bg-ls-0 dark:bg-ds-900">
+	<div
+		class="flex flex-col py-8 pl-16 pr-8 border-l border-ls-200 dark:border-ds-700 select-none bg-ls-0 dark:bg-ds-900">
 
 		<SectionTitle :label="playlist.name || 'New Playlist'">
 			<template #right>
