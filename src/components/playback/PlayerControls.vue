ad2ea198695152a0d1265e9ee8eb12b8207ad673
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-31 13:22:43 -0700

    Controls style improvements and volume WIP

--- src/components/playback/PlayerControls.vue
+++ src/components/playback/PlayerControls.vue
@@ -2,31 +2,59 @@
     <div class="flex gap-4 items-center justify-end">
         <Spinner v-if="debouncedBuffering" />
         <div class="flex gap-2 items-center">
-            <span @click="skipPrevious" class="material-icons-round text-3xl"
-                :class="playlist.hasPrevious() ? 'cursor-pointer text-ls-900' : 'active:pointer-events-none cursor-not-allowed text-ls-400'">skip_previous</span>
-            <div @click="togglePlayback"
-                class="cursor-pointer material-icons-round text-4xl rounded-full bg-accent-600 text-ls-0 text-center py-1 w-12 h-12 hover:bg-accent-500">
+
+            <span @click="skipPrevious" class="material-icons-round w-9 h-9 mb-0.5
+                text-3xl text-center
+                rounded-lg border border-ls-0
+                active:py-px" :class="playlist.hasPrevious()
+                    ? 'cursor-pointer text-ls-900 hover:border-ls-200 hover:bg-ls-50'
+                    : 'active:pointer-events-none cursor-not-allowed text-ls-400'">
+                skip_previous
+            </span>
+
+            <div @click="togglePlayback" class="cursor-pointer material-icons-round py-1 w-12 h-12 
+                text-4xl text-center text-ls-0
+                rounded-full bg-accent-600 
+                hover:bg-accent-500 hover:ring-2
+                ring-accent-500 ring-offset-4
+                active:pt-[5px]">
                 {{ paused ? 'play_arrow' : 'pause' }}
             </div>
-            <span @click="skipNext" class="material-icons-round text-3xl"
-                :class="playlist.hasNext() ? 'cursor-pointer text-ls-900' : 'active:pointer-events-none cursor-not-allowed text-ls-400'">skip_next</span>
+
+            <span @click="skipNext" class="material-icons-round w-9 h-9 mb-0.5
+                text-3xl text-center
+                rounded-lg border border-ls-0
+                active:pt-px" :class="playlist.hasNext()
+                    ? 'cursor-pointer text-ls-900 hover:border-ls-200 hover:bg-ls-50'
+                    : 'active:pointer-events-none cursor-not-allowed text-ls-400'">
+                skip_next
+            </span>
+
         </div>
-        <div class="flex items-center">
-            <!-- TODO volume interactions -->
-            <span class="cursor-pointer material-icons-round">{{ muted ? 'volume_off' : 'volume_down' }}</span>
-            <div class="w-28 h-1.5 bg-accent-600 rounded-full" />
+
+        <div class="flex items-center gap-0.5">
+            <span @click="toggleMute" class="cursor-pointer material-icons-round"
+                :class="isQuiet ? '-translate-x-0.5 text-ls-700' : 'text-ls-900'">
+                {{ isQuiet ? 'volume_mute' : 'volume_down' }}
+            </span>
+            <Slider class="w-20" v-model="volume" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { refDebounced } from "@vueuse/core";
 
+import Slider from "@/components/basic/Slider.vue";
 import Spinner from "@/components/basic/Spinner.vue";
 import { usePlaylistStore } from "@/stores/playlist";
 
 const playlist = usePlaylistStore();
+
+const volume = ref(0);
+
+const isQuiet = computed(() => props.muted || volume.value == 0);
 
 const props = defineProps<{
     buffering: boolean,
@@ -66,5 +94,9 @@ async function skipNext() {
     if (newTrack?.key == oldTrack?.key) {
         emit("restart");
     }
+}
+
+function toggleMute() {
+
 }
 </script>
