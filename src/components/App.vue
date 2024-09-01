cd95f55e75b8e039fc9f863547aba9c607ca13fc
Antoine Gersant <antoine.gersant@lesforges.org>
2024-08-31 19:09:03 -0700

    Cosmetic change

--- src/components/App.vue
+++ src/components/App.vue
@@ -4,58 +4,16 @@
 		<div class="flex flex-1 min-h-0">
 			<Sidebar class="shrink-0 m-8 mr-0" />
 			<div class="grow grid grid-cols-2">
-				<router-view class="px-20 py-8 min-h-0" />
+				<router-view class="px-16 py-8 min-h-0" />
 				<Playlist class="min-h-0" />
 			</div>
 		</div>
-		<div
-			class="px-8 py-4 border-t border-ls-200 dark:border-ds-700 flex-none flex items-center gap-16 bg-ls-0 dark:bg-ds-900">
-			<div class="flex">
-				<div class="aspect-square h-20 rounded-md bg-pink-400" />
-				<div class="ml-4 flex flex-col justify-center">
-					<div class="font-semibold">No Turning Back</div>
-					<div class="text-sm">Stratovarius</div>
-				</div>
-			</div>
-			<div class="flex gap-4 items-center">
-				<span class="material-icons-round text-4xl">skip_previous</span>
-				<div class="material-icons-round text-2xl rounded-full p-2 border-4 px-3">pause</div>
-				<span class="material-icons-round text-4xl">skip_next</span>
-			</div>
-			<div class="grow flex items-center gap-4 text-xs">
-				<span>0:24</span>
-				<div id="waveform" class="grow" />
-				<span>5:46</span>
-			</div>
-			<div class="flex items-center gap-4">
-				<span class="material-icons-round">volume_down</span>
-				<Slider class="w-32" />
-			</div>
-		</div>
+		<Player class="border-t border-ls-200 dark:border-ds-700" />
 	</div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-import WaveSurfer from 'wavesurfer.js'
-
-import { makeAudioURL } from '@/api/endpoints';
 import Sidebar from './Sidebar.vue';
+import Player from './playback/Player.vue';
 import Playlist from './playback/Playlist.vue';
-
-onMounted(() => {
-	// TODO major hitch when agressively resizing browser window horizontally
-	WaveSurfer.create({
-		container: '#waveform',
-		url: makeAudioURL("root/Jazz/Casiopea/1981 - Cross Point/02 - Swear.mp3"),
-		// waveColor: $dt("text.muted.color").value.light.value,
-		// progressColor: $dt("primary.color").value.light.value,
-		height: 80,
-		barWidth: 3,
-		barGap: 2,
-		barRadius: 4,
-		barHeight: .8,
-		cursorWidth: 0,
-	})
-});
 </script>
--- src/components/playback/Player.vue
+++ src/components/playback/Player.vue
@@ -1,88 +1,80 @@
 <template>
-	<div class="player">
-		<audio ref="htmlAudio" controls v-if="trackURL" v-bind:src="trackURL" @timeupdate="onTimeUpdate"
-			@error="onPlaybackError" @ended="onEnded" @pause="onPaused" @playing="onPlaying"
-			@waiting="onWaiting"></audio>
+	<div class="relative flex items-center px-8 py-4 bg-ls-0 dark:bg-ds-900 whitespace-nowrap select-none">
+		<audio ref="htmlAudio" v-if="audioURL" v-bind:src="audioURL" @timeupdate="onTimeUpdate" @error="onPlaybackError"
+			@ended="onEnded" @pause="onPaused" @playing="onPlaying" @waiting="onWaiting" />
 
-		<div v-if="currentTrack" class="controls noselect">
-			<div class="playback">
-				<div class="control previous" v-on:click="skipPrevious">
-					<i class="material-icons md-18">skip_previous</i>
-				</div>
-				<div v-if="paused" class="control play" v-on:click="togglePlay">
-					<i class="material-icons">play_arrow</i>
-				</div>
-				<div v-if="!paused" class="control pause" v-on:click="togglePlay">
-					<i class="material-icons">pause</i>
-				</div>
-				<div class="control next" v-on:click="skipNext">
-					<i class="material-icons md-18">skip_next</i>
-				</div>
+		<PlayerAlbum class="basis-80 min-w-0 grow shrink" />
+
+		<!-- Waveform -->
+		<div class="grow-[8] basis-80 min-w-32 mx-16 flex flex-col gap-4">
+			<div class="flex justify-center text-sm gap-1 text-ls-900">
+				<span class="text-accent-600 underline">
+					<span v-if="song && song.artists?.length">
+						{{ formatArtists(song.artists) }}
+					</span>
+					<span v-else-if="song && song.album_artists?.length">{{ formatArtists(song.album_artists)
+						}}</span>
+				</span>
+				<span>-</span>
+				<span v-if="song" class="overflow-hidden text-ellipsis select-text">
+					{{ formatTitle(song) }}
+				</span>
+				<span v-else-if="currentTrack" class="select-text">{{ getPathTail(currentTrack.path) }}</span>
+				<span v-else class="mb-4 rounded-full w-40 h-3 bg-ls-200" />
 			</div>
-			<div class="volume">
-				<div class="icon" v-on:click="toggleMute">
-					<i v-if="volume == 0" class="material-icons">volume_off</i>
-					<i v-if="volume > 0" class="material-icons">volume_down</i>
-				</div>
-				<div class="bar" ref="volumeInput" v-on:mousedown="volumeMouseDown">
-					<div class="fill" v-bind:style="{ width: 100 * volume + '%' }"></div>
-				</div>
+			<div class="flex grow items-center gap-4 text-xs text-ls-700">
+				<span>{{ currentTrack ? formatDuration(secondsPlayed) : "-:--" }}</span>
+				<div id="waveform" class="grow" />
+				<span>{{ song && song.duration ? formatDuration(song.duration) : "-:--" }}</span>
 			</div>
+
 		</div>
 
-		<div v-if="currentTrack" class="art">
-			<CoverArt v-if="artworkURL" v-bind:url="artworkURL" />
-			<div v-if="!artworkURL" class="missing-art"></div>
-		</div>
+		<PlayerControls class="basis-80 min-w-0 grow shrink" v-model:volume="volume" :paused="paused"
+			:buffering="buffering" @play="play" @pause="pause" @restart="playFromStart" />
 
-		<div class="currentTrack" v-if="currentTrack">
-			<div class="trackInfo">
-				<div class="primary">
-					<Spinner class="spinner" v-if="debouncedBuffering" />{{ trackInfoPrimary }}
-				</div>
-				<div class="secondary">{{ trackInfoSecondary }}</div>
-			</div>
-			<div class="seekBar" ref="seekInput" v-on:mousedown="seekMouseDown">
-				<div class="fill" v-bind:style="{ width: 100 * trackProgress + '%' }"></div>
-				<div class="head" v-bind:style="{ left: 100 * trackProgress + '%' }"></div>
-			</div>
-			<div v-if="currentTrack" class="trackDuration">{{ formatDuration(secondsPlayed) }}</div>
-		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { computed, nextTick, onBeforeUnmount, onMounted, Ref, ref, watch } from "vue";
-import { refDebounced } from "@vueuse/core";
-import notify from "@/notify";
-import { loadForCurrentUser, saveForCurrentUser } from "@/disk";
+import { computed, nextTick, onMounted, Ref, ref, watch } from "vue";
+import WaveSurfer from 'wavesurfer.js'
+
 import { lastFMNowPlaying, lastFMScrobble, makeAudioURL, makeThumbnailURL } from "@/api/endpoints";
+import PlayerAlbum from "@/components/playback/PlayerAlbum.vue";
+import PlayerControls from "@/components/playback/PlayerControls.vue";
+import { loadForCurrentUser, saveForCurrentUser } from "@/disk";
+import { formatArtists, formatDuration, formatTitle, getPathTail } from "@/format";
+import notify from "@/notify";
 import { usePlaylistStore } from "@/stores/playlist";
 import { usePreferencesStore } from "@/stores/preferences";
-import { formatArtists, formatDuration, formatTitle } from "@/format";
-import CoverArt from "@/components/CoverArt.vue";
-import Spinner from "@/components/Spinner.vue";
+import { useSongsStore } from "@/stores/songs";
 
 const playlist = usePlaylistStore();
+const songs = useSongsStore();
 const preferences = usePreferencesStore();
 
 const volume = ref(1);
-const unmutedVolume = ref(1);
 const secondsPlayed = ref(0);
 const duration = ref(1);
-const adjusting: Ref<"volume" | "seek" | null> = ref(null);
 const paused = ref(true);
 const buffering = ref(false);
-const debouncedBuffering = refDebounced(buffering, 100);
 const canScrobble = ref(false);
-const mounted = ref(false);
 const htmlAudio: Ref<HTMLAudioElement | null> = ref(null);
-const seekInput: Ref<HTMLElement | null> = ref(null);
-const volumeInput: Ref<HTMLElement | null> = ref(null);
 
 const currentTrack = computed(() => playlist.currentTrack);
-const trackURL = computed(() => currentTrack.value ? makeAudioURL(currentTrack.value.path) : null);
-const artworkURL = computed(() => currentTrack.value && currentTrack.value.artwork ? makeThumbnailURL(currentTrack.value.artwork) : null);
+
+const song = computed(() => {
+	if (!currentTrack.value) {
+		return undefined;
+	}
+	return songs.cache.get(currentTrack.value.path)
+});
+
+const audioURL = computed(() => currentTrack.value ? makeAudioURL(currentTrack.value.path) : null);
+const artworkURL = computed(() => song.value && song.value.artwork ? makeThumbnailURL(song.value.artwork, "small") : null);
+
+let wavesurfer: WaveSurfer | undefined;
 
 const trackProgress = computed(() => {
 	if (isNaN(duration.value)) {
@@ -94,34 +86,8 @@ const trackProgress = computed(() => {
 	return Math.max(0, Math.min(secondsPlayed.value / duration.value, 1));
 });
 
-const trackInfoPrimary = computed(() => {
-	const track = currentTrack.value;
-	if (!track) {
-		return "";
-	}
-	let result = formatArtists(track.artists || []) || "Unknown Artist";
-	result += " - ";
-	result += formatTitle(track);
-	return result;
-});
-
-const trackInfoSecondary = computed(() => {
-	const track = currentTrack.value;
-	if (!track) {
-		return "";
-	}
-	let result = track.album || "Unknown Album";
-	if (track.year) {
-		result += " (" + track.year + ")";
-	}
-	if (track.track_number) {
-		result += " #" + track.track_number;
-	}
-	return result;
-});
-
-watch(currentTrack, (to, from) => {
-	if (!mounted.value) {
+watch(audioURL, (to, from) => {
+	if (!wavesurfer || !audioURL.value) {
 		return;
 	}
 	handleCurrentTrackChanged();
@@ -135,19 +101,20 @@ watch(volume, (to, from) => {
 	saveForCurrentUser("volume", to);
 });
 
-onBeforeUnmount(() => {
-	mounted.value = false;
-});
-
 onMounted(() => {
-	mounted.value = true;
+	wavesurfer = WaveSurfer.create({
+		container: '#waveform',
+		height: 32,
+		cursorWidth: 0,
+		waveColor: "#e5e7eb", // TODO theming
+		progressColor: "#46c8f1", // TODO theming
+		media: htmlAudio.value as HTMLMediaElement,
+		dragToSeek: true,
+	})
 
 	const savedVolume = parseFloat(loadForCurrentUser("volume"));
 	if (!isNaN(savedVolume)) {
 		volume.value = savedVolume;
-		if (savedVolume > 0) {
-			unmutedVolume.value = savedVolume;
-		}
 	}
 
 	if (currentTrack.value) {
@@ -162,36 +129,13 @@ onMounted(() => {
 		navigator.mediaSession.setActionHandler("previoustrack", skipPrevious);
 		navigator.mediaSession.setActionHandler("nexttrack", skipNext);
 	}
-
-	// Global mouse handling
-	let onMouseMove = (event: MouseEvent) => {
-		if (event.buttons != undefined) {
-			const isLeftClickDown = (event.buttons & 1) == 1;
-			if (!isLeftClickDown) {
-				return;
-			}
-		}
-		if (adjusting.value == "volume") {
-			volumeMouseMove(event);
-		} else if (adjusting.value == "seek") {
-			seekMouseMove(event);
-		}
-	};
-
-	document.body.addEventListener("mousemove", onMouseMove);
-	document.body.addEventListener("mousedown", onMouseMove);
-	document.body.addEventListener("mouseup", () => {
-		if (adjusting.value == "volume" && volume.value != 0) {
-			unmutedVolume.value = volume.value;
-		}
-		adjusting.value = null;
-	});
 });
 
 function handleCurrentTrackChanged() {
-	if (adjusting.value == "seek") {
-		adjusting.value = null;
-	}
+	// TODO wavesurfer doesnt handle this!!
+	// if (adjusting.value == "seek") {
+	// 	adjusting.value = null;
+	// }
 	canScrobble.value = true;
 	updateMediaSession();
 	updateWindowTitle();
@@ -253,26 +197,21 @@ function updateWindowTitle() {
 	document.title = windowTitle;
 }
 
-function toggleMute() {
-	if (volume.value != 0) {
-		volume.value = 0;
-	} else {
-		volume.value = unmutedVolume.value;
-	}
-}
-
-function togglePlay() {
+function play() {
 	if (!htmlAudio.value) {
 		return;
 	}
-	if (paused.value) {
-		htmlAudio.value.play();
-		paused.value = false;
-	} else {
-		htmlAudio.value.pause();
-		paused.value = true;
-		buffering.value = false;
+	htmlAudio.value.play();
+	paused.value = false;
+}
+
+function pause() {
+	if (!htmlAudio.value) {
+		return;
 	}
+	htmlAudio.value.pause();
+	paused.value = true;
+	buffering.value = false;
 }
 
 async function skipPrevious() {
@@ -304,45 +243,12 @@ function updateScrobble() {
 	}
 }
 
-function seekMouseDown() {
-	adjusting.value = "seek";
-}
-
-function volumeMouseDown() {
-	adjusting.value = "volume";
-}
-
-function seekMouseMove(event: MouseEvent) {
-	if (adjusting.value == "seek" && seekInput.value) {
-		let x = event.pageX;
-		let o: HTMLElement | null = seekInput.value;
-		while (o) {
-			x -= o.offsetLeft;
-			o = o.offsetParent as (HTMLElement | null);
-		}
-		let progress = Math.min(Math.max(x / seekInput.value.offsetWidth, 0), 1);
-		seekTo(progress * duration.value);
-	}
-}
-
 function seekTo(seconds: number) {
 	if (!htmlAudio.value) {
 		return;
 	}
 	htmlAudio.value.currentTime = seconds;
 	canScrobble.value = false;
-}
-
-function volumeMouseMove(event: MouseEvent) {
-	if (adjusting.value == "volume" && volumeInput.value) {
-		let x = event.pageX;
-		let o: HTMLElement | null = volumeInput.value;
-		while (o) {
-			x -= o.offsetLeft;
-			o = o.offsetParent as (HTMLElement | null);
-		}
-		volume.value = Math.min(Math.max(x / volumeInput.value.offsetWidth, 0), 1);
-	}
 }
 
 function onEnded(event: Event) {
@@ -366,7 +272,7 @@ function onWaiting(event: Event) {
 }
 
 function onTimeUpdate(event: Event) {
-	if (!mounted.value || !htmlAudio.value) {
+	if (!htmlAudio.value) {
 		return;
 	}
 	const newTime = htmlAudio.value.currentTime;
@@ -408,157 +314,3 @@ function onPlaybackError(event: Event) {
 	skipNext();
 }
 </script>
-
-<style scoped>
-.player {
-	padding: 40px;
-	display: flex;
-	flex-flow: row nowrap;
-	align-items: center;
-	justify-content: center;
-	overflow: hidden;
-}
-
-audio {
-	display: none;
-}
-
-.art {
-	width: 120px;
-	height: 120px;
-	position: relative;
-}
-
-.missing-art {
-	height: 100%;
-	border-radius: 5px;
-	background: repeating-linear-gradient(-45deg, var(--theme-background-muted), var(--theme-background-muted) 8px, var(--theme-background) 8px, var(--theme-background) 16px);
-}
-
-.currentTrack {
-	flex-grow: 1;
-	padding-left: 20px;
-}
-
-.controls {
-	margin-right: 20px;
-	width: 120px;
-	cursor: default;
-}
-
-.controls .playback {
-	display: flex;
-	flex-flow: row nowrap;
-	justify-content: space-between;
-	align-items: center;
-}
-
-.control {
-	border-radius: 50%;
-	border: 1px solid var(--theme-border);
-	text-align: center;
-}
-
-.control.previous,
-.control.next {
-	width: 28px;
-	height: 28px;
-	line-height: 28px;
-	padding-top: 4px;
-	box-sizing: border-box;
-}
-
-.control.play,
-.control.pause {
-	padding-top: 6px;
-	width: 40px;
-	height: 40px;
-	line-height: 40px;
-	box-sizing: border-box;
-}
-
-.volume {
-	margin-left: -4px;
-	display: flex;
-	flex-flow: row nowrap;
-}
-
-.volume .bar {
-	flex-grow: 1;
-	background-color: var(--theme-foreground-muted);
-	height: 10px;
-	margin: 7px 0;
-	border-radius: 3px;
-}
-
-.volume .fill {
-	height: 100%;
-	max-width: 100%;
-	background-color: var(--theme-accent);
-	border-radius: 3px;
-}
-
-.trackInfo .primary {
-	font-weight: 600;
-	margin-bottom: -5px;
-}
-
-.spinner {
-	margin-bottom: -2px;
-	margin-right: 8px;
-}
-
-.trackInfo .secondary,
-.trackDuration {
-	font-weight: 300;
-	font-size: 0.875rem;
-}
-
-.seekBar {
-	width: 100%;
-	background-color: var(--theme-foreground-muted);
-	height: 10px;
-	margin: 6px 0;
-	border-radius: 3px;
-}
-
-.seekBar .fill {
-	height: 100%;
-	width: 0;
-	max-width: 100%;
-	background-color: var(--theme-accent);
-	border-radius: 3px;
-}
-
-.seekBar .head {
-	width: 16px;
-	height: 16px;
-	position: relative;
-	top: -14px;
-	margin-left: -9px;
-	background-color: var(--theme-background);
-	border: 1px solid var(--theme-border);
-	border-radius: 3px;
-}
-
-.controls,
-.art img,
-.currentTrack {
-	animation-duration: 250ms;
-	animation-name: fadein;
-}
-
-@keyframes fadein {
-	from {
-		margin-top: 100px;
-		transform: scale(0);
-		opacity: 0;
-	}
-
-	to {
-		margin-top: 0;
-		transform: scale(1);
-		opacity: 1;
-	}
-}
-</style>
