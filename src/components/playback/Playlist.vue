623d3eae4ccadc6f08ae627fa36ca3be518e7490
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-01 12:48:04 -0700

    Minor spacing adjustments w/ scrollbar

--- src/components/playback/Playlist.vue
+++ src/components/playback/Playlist.vue
@@ -25,9 +25,9 @@
 		</div>
 
 		<div class="grow relative min-h-0">
-			<OrderableList ref="orderableList" class="h-full -mx-8" :items="playlist.entries" :item-height="itemHeight"
-				:show-drop-preview="dragPayload != undefined" @keydown="onKeyDown" @list-reorder="onReorder"
-				@list-delete="playlist.removeTracks" @list-drop="onDrop">
+			<OrderableList ref="orderableList" class="h-full -ml-8 -mr-2" :items="playlist.entries"
+				:item-height="itemHeight" :show-drop-preview="dragPayload != undefined" @keydown="onKeyDown"
+				@list-reorder="onReorder" @list-delete="playlist.removeTracks" @list-drop="onDrop">
 				<template #default="{ item, index, selected, focused }">
 					<PlaylistSong :entry="item" :compact="compact" :height="itemHeight" :index="index"
 						:selected="selected" :focused="focused" />
