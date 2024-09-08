81f58e02b6d97900b864121d6abde7f515d70b49
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-08 14:00:19 -0700

    Global select-none

--- src/components/playback/Playlist.vue
+++ src/components/playback/Playlist.vue
@@ -1,7 +1,6 @@
 <template>
 
-	<div
-		class="flex flex-col py-8 pl-16 pr-8 border-l border-ls-200 dark:border-ds-700 select-none bg-ls-0 dark:bg-ds-900">
+	<div class="flex flex-col py-8 pl-16 pr-8 border-l border-ls-200 dark:border-ds-700 bg-ls-0 dark:bg-ds-900">
 
 		<SectionTitle :label="playback.name || 'New Playlist'">
 			<template #right>
