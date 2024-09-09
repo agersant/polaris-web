62e67b3cbdfb52a55b42e3da659f2e36683af008
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-08 18:10:09 -0700

    Adds support for button groups

---
+++ src/components/basic/ButtonGroup.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex rounded-md shadow-sm
+        [&>button]:rounded-none [&>button:first-child]:rounded-l-md [&>button:last-child]:rounded-r-md
+        [&>button]:-ml-px [&>button:first-child]:ml-0">
+        <slot />
+    </div>
+</template>
