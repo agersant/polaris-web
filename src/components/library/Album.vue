d894af63524e9a4babd36f83efa7bc238690bc23
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-14 12:47:40 -0700

    Album page draft

---
+++ src/components/library/Album.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="flex flex-col whitespace-nowrap">
+		<PageTitle :label="header" />
+		<div class="mb-8">
+			<span v-for="artist of albumKey.artists">
+				{{ artist }}
+			</span>
+		</div>
+		<div v-if="fetchedAlbum" class="grow min-h-0 flex flex-col">
+			<div class="min-h-0 flex items-start gap-8">
+				<div class="basis-2/5">
+					<AlbumArt :url="artworkURL" />
+				</div>
+				<div class="grow -mr-4 pr-4 self-stretch overflow-scroll flex flex-col">
+					<div v-for="song of fetchedAlbum.songs" v-text="formatTitle(song)" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed, } from "vue";
+import { useAsyncState, watchImmediate } from "@vueuse/core";
+
+import { AlbumKey } from "@/api/dto";
+import { getAlbum, makeThumbnailURL } from "@/api/endpoints";
+import AlbumArt from '@/components/AlbumArt.vue';
+import PageTitle from '@/components/basic/PageTitle.vue';
+import { formatTitle } from "@/format";
+
+/* TODOS
+Genres
+Year
+Disc headers
+Song artists
+Artist links
+Loading state
+Error state
+Play buttons
+Art drag and drop
+Song multiselect
+Song drag and drop
+Dark mode
+Context menus
+*/
+
+const props = defineProps<{ albumKey: AlbumKey }>();
+
+const { state: fetchedAlbum, isLoading, error, execute: fetchAlbum } = useAsyncState(
+	(key: AlbumKey) => getAlbum(key),
+	undefined,
+	{ immediate: false, resetOnExecute: true }
+);
+
+watchImmediate(() => props.albumKey, () => {
+	fetchAlbum(0, props.albumKey);
+});
+
+const header = computed((): string => {
+	return props.albumKey.name || "Unknown Album";
+});
+
+const artworkURL = computed(() => fetchedAlbum.value?.artwork ? makeThumbnailURL(fetchedAlbum.value.artwork, "small") : undefined);
+</script>
