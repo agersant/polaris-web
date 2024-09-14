459cd37e817070358d1cbde3554244efe87d09e2
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-14 13:31:07 -0700

    Adds album art shadow

--- src/components/AlbumArt.vue
+++ src/components/AlbumArt.vue
@@ -1,14 +1,14 @@
 <template>
-    <UseImage :src="url || ''" class="w-full h-full object-cover rounded-md">
+    <UseImage :src="url || ''" class="w-full h-full object-cover" :class="rounding">
         <template #loading>
-            <div
-                class="bg-ls-200 text-ls-400 dark:bg-ds-800 dark:text-ds-600 rounded-md w-full h-full flex items-center justify-center">
+            <div class="bg-ls-200 text-ls-400 dark:bg-ds-800 dark:text-ds-600 w-full h-full flex items-center justify-center"
+                :class="rounding">
                 <Spinner />
             </div>
         </template>
 
         <template #error>
-            <div class="bg-ls-200 dark:bg-ds-800 rounded-md w-full h-full flex items-center justify-center">
+            <div class="bg-ls-200 dark:bg-ds-800 w-full h-full flex items-center justify-center" :class="rounding">
                 <span class="material-icons-round text-ls-400 dark:text-ds-700">image_not_supported</span>
             </div>
         </template>
@@ -16,11 +16,15 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { UseImage } from "@vueuse/components";
 
 import Spinner from "@/components/basic/Spinner.vue"
 
-defineProps<{
+const { size = "md", ...props } = defineProps<{
     url: string | undefined,
+    size?: "md" | "lg",
 }>();
+
+const rounding = computed(() => size == "md" ? "rounded-md" : "rounded-lg");
 </script>
