4456ffeef22f949f42364b43f9a304b681c59047
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-14 18:34:51 -0700

    Revert title tracking

--- src/components/basic/PageTitle.vue
+++ src/components/basic/PageTitle.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="mt-11 mb-8 pb-3 border-b border-ls-200 dark:border-ds-700 flex items-center justify-between">
         <div class="min-w-0 flex items-center">
-            <h3
-                class="text-4xl tracking-wider text-ls-500 dark:text-ds-400 whitespace-nowrap overflow-hidden text-ellipsis">
+            <h3 class="text-4xl text-ls-500 dark:text-ds-400 whitespace-nowrap overflow-hidden text-ellipsis">
                 {{ label }}
             </h3>
             <slot name="left" />
