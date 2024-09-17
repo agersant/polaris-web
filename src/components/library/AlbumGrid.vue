13c800d5ec18fdef7f15814d4a3094b4d651033e
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-16 23:18:01 -0700

    Albums page WIP

--- src/components/library/AlbumGrid.vue
+++ src/components/library/AlbumGrid.vue
@@ -1,56 +1,61 @@
 <template>
-    <div class="flex flex-wrap content-start gap-y-8" :style="`column-gap: ${gapSize}px`">
-        <div v-for="album of albums" class="flex flex-col gap-2" :style="itemStyle">
-            <div @click="router.push(makeAlbumURL(album.main_artists, album.name))" :class="itemClass" class="
-                cursor-pointer aspect-square w-full origin-center 
-                transition-all ease-out duration-100
-                hover:opacity-90
-                ">
-                <Draggable :make-payload="() => new DndPayloadAlbumHeader(album)">
-                    <AlbumArt :url="album.artwork ? makeThumbnailURL(album.artwork, 'small') : undefined" />
-                    <template #drag-preview>
-                        <AlbumDragPreview :album="album" />
-                    </template>
-                </Draggable>
-            </div>
-            <div class="flex flex-col gap-1">
-                <div class="font-medium text-sm line-clamp-2 text-ls-900 dark:text-ds-200" v-text="album.name" />
-                <div class="font-medium text-sm text-ls-500 dark:text-ds-400" v-text="album.year" />
-            </div>
+    <div ref="viewport" class="overflow-y-scroll -m-4 p-4 mb-0 pb-0" :style="{
+        gap: `${gapSize}px`,
+    }">
+        <div class="relative" :style="{ height: `${contentHeight}px` }">
+            <AlbumGridCell ref="sampleCell" :album="sampleAlbum" :size="cellSize" :show-artists="showArtists"
+                class="absolute opacity-0" :style="{ width: `${itemWidth}px` }" />
+            <AlbumGridCell v-for="(album, index) of virtualAlbums" :key="album.name + album.main_artists.join('')"
+                :album="album" :size="cellSize" :show-artists="showArtists" class="absolute" :style="{
+                    width: `${itemWidth}px`,
+                    left: `${((index + firstVirtual) % numColumns) * (itemWidth + gapSize)}px`,
+                    top: `${Math.floor((index + firstVirtual) / numColumns) * (itemHeight + gapSize)}px`,
+                }" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { useRouter } from "vue-router";
+import { computed, ref, useTemplateRef } from 'vue';
+import { useElementSize, useScroll } from '@vueuse/core';
 
-import AlbumArt from '@/components/AlbumArt.vue';
-import Draggable from '@/components/basic/Draggable.vue';
-import AlbumDragPreview from '@/components/library/AlbumDragPreview.vue';
 import { AlbumHeader } from '@/api/dto';
-import { makeThumbnailURL } from '@/api/endpoints';
-import { DndPayloadAlbumHeader } from '@/dnd';
-import { makeAlbumURL } from '@/router';
-
-const router = useRouter();
+import AlbumGridCell from '@/components/library/AlbumGridCell.vue';
 
 const props = defineProps<{
     albums: AlbumHeader[],
+    showArtists: boolean,
     numColumns: number,
 }>();
 
 const gapSize = ref(32);
 
-const itemClass = computed(() => {
-    return [
-        props.numColumns <= 3 ? 'hover:scale-105' : 'hover:scale-110',
-    ];
+const cellSize = computed(() =>
+    props.numColumns <= 3 ? "lg" : "md",
+);
+
+const viewport = useTemplateRef("viewport");
+const { width: viewportWidth, height: viewportHeight } = useElementSize(viewport);
+const { y: scrollY } = useScroll(viewport);
+
+const sampleCell = useTemplateRef("sampleCell");
+const itemWidth = computed(() =>
+    (viewportWidth.value - (props.numColumns - 1) * gapSize.value) / props.numColumns
+);
+const { height: itemHeight } = useElementSize(sampleCell);
+
+const contentHeight = computed(() => Math.ceil(props.albums.length / props.numColumns) * (itemHeight.value + gapSize.value));
+
+const firstVirtual = computed(() => Math.floor(scrollY.value / (itemHeight.value + gapSize.value)) * props.numColumns);
+const numVirtualItems = computed(() => Math.ceil(1 + viewportHeight.value / (itemHeight.value + gapSize.value)) * props.numColumns);
+
+const virtualAlbums = computed(() => {
+    return props.albums.slice(firstVirtual.value, firstVirtual.value + numVirtualItems.value);
 });
 
-const itemStyle = computed(() => {
-    return {
-        width: `calc(${100 / props.numColumns}% - ${gapSize.value * (props.numColumns - 1) / props.numColumns}px)`,
-    }
-});
+const sampleAlbum = {
+    name: "WWWW ".repeat(10),
+    main_artists: ["WWWW ".repeat(10)],
+    year: 2000,
+};
 </script>
---
+++ src/components/library/AlbumGridCell.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="flex flex-col gap-2">
+        <div @click="router.push(makeAlbumURL(album.main_artists, album.name))"
+            :class="size == 'lg' ? 'hover:scale-105' : 'hover:scale-110'" class="
+                cursor-pointer aspect-square w-full origin-center 
+                transition-all ease-out duration-100
+                hover:opacity-90
+                ">
+            <Draggable :make-payload="() => new DndPayloadAlbumHeader(album)">
+                <AlbumArt :url="album.artwork ? makeThumbnailURL(album.artwork, 'small') : undefined" />
+                <template #drag-preview>
+                    <AlbumDragPreview :album="album" />
+                </template>
+            </Draggable>
+        </div>
+        <div class="font-medium text-sm whitespace-normal text-ls-500 dark:text-ds-400">
+            <span v-text="album.name" class="mb-1 line-clamp-2 text-ls-900 dark:text-ds-200" />
+            <span v-if="showArtists" class="line-clamp-1">
+                <span v-for="(artist, index) of album.main_artists">
+                    <span v-text="artist" />
+                    <span v-if="index < album.main_artists.length - 1" v-text="`, `" />
+                </span>
+            </span>
+            <span v-if="!showArtists" v-text="album.year" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+import { AlbumHeader } from '@/api/dto';
+import { makeThumbnailURL } from '@/api/endpoints';
+import AlbumArt from '@/components/AlbumArt.vue';
+import Draggable from '@/components/basic/Draggable.vue';
+import AlbumDragPreview from '@/components/library/AlbumDragPreview.vue';
+import { DndPayloadAlbumHeader } from '@/dnd';
+import { makeAlbumURL } from '@/router';
+
+const router = useRouter();
+
+defineProps<{
+    album: AlbumHeader,
+    showArtists: boolean,
+    size: "md" | "lg",
+}>();
+
+</script>
