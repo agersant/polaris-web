2b29a79eae2661392f0c762de935d22c13d9d7de
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-22 17:41:34 -0700

    Extract SongListRow out of PlaylistSong

---
+++ src/components/SongListRow.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="relative grow h-full min-w-0 rounded-sm flex items-center" :class="rowClass">
+        <div v-if="!compact" class="basis-10 h-10 mr-3 shrink-0 flex items-center">
+            <AlbumArt :url="artworkURL" />
+        </div>
+        <div class="grow basis-0 pr-4 text-ellipsis" :class="{ 'overflow-hidden': song }">
+            <span v-if="song" v-text="formatTrackContext(song)" />
+            <div v-else class="-mr-8 bg-black/5 dark:bg-white/5 h-3 rounded-full" />
+        </div>
+        <div class="basis-8 shrink-0 text-right mr-1">
+            <span v-if="song && song.track_number"> {{ formatTrackNumber(song) }}.</span>
+        </div>
+        <div class="grow basis-0 pr-4 overflow-hidden text-ellipsis">
+            <span v-if="song">
+                {{ formatTitle(song) }}
+                <span :class="selected ? '' : 'text-ls-400 dark:text-ds-600'"
+                    v-if="song.artists && song.album_artists && !equals(song.artists, song.album_artists)">
+                    ({{ formatArtists(song.artists) }})
+                </span>
+            </span>
+            <div v-else class="bg-black/5 dark:bg-white/5 h-3 rounded-full" />
+        </div>
+        <div class="basis-16 shrink-0 text-right">
+            <span v-if="song">{{ formatTrackDuration(song) }}</span>
+            <div v-else class="bg-black/5 dark:bg-white/5 h-3 rounded-full" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import equals from "array-equal"
+import { computed } from 'vue';
+
+import { Song } from "@/api/dto";
+import { makeThumbnailURL } from "@/api/endpoints";
+import AlbumArt from '@/components/AlbumArt.vue';
+import { formatArtists, formatDuration, formatTitle, formatTrackNumber } from '@/format';
+import { useSongsStore } from '@/stores/songs';
+
+const songs = useSongsStore();
+
+const props = defineProps<{
+    path: string,
+    index: number,
+    compact: boolean,
+    selected: boolean,
+    focused: boolean,
+    isCurrent?: boolean,
+}>();
+
+const song = computed(() => songs.cache.get(props.path));
+
+const artworkURL = computed(() => song.value?.artwork ? makeThumbnailURL(song.value.artwork, "tiny") : undefined);
+
+const rowClass = computed(() => {
+
+    const isOdd = props.index % 2 == 1;
+
+    let background;
+    if (props.selected) {
+        background = "bg-accent-100 dark:bg-accent-900";
+    } else if (props.compact) {
+        background = isOdd ? "bg-ls-50 hover:bg-ls-100 dark:bg-ds-800/20 dark:hover:bg-ds-800" : "bg-ls-0 hover:bg-ls-100 dark:bg-ds-900 dark:hover:bg-ds-800";
+    } else {
+        background = [
+            "bg-gradient-to-r from-ls-0 dark:from-ds-900 to-[50px] hover:to-ls-100 dark:hover:to-ds-800",
+            isOdd ? "to-ls-50 dark:to-ds-800/20" : "to-ls-0 dark:to-ds-900",
+        ];
+    }
+
+    let text;
+    if (props.isCurrent && props.selected) {
+        text = "text-accent-700 dark:text-accent-100";
+    } else if (props.selected) {
+        text = "text-accent-700 dark:text-accent-200";
+    } else if (props.isCurrent) {
+        text = "text-ls-700 dark:text-ds-0";
+    } else {
+        text = "text-ls-700 dark:text-ds-400";
+    }
+
+    return [
+        text,
+        background,
+        props.isCurrent ? "font-semibold" : "",
+        props.compact ? "px-3" : "pr-2",
+        !props.compact && props.selected ? "-ml-2 pl-2" : "",
+        props.focused ? "outline-1 outline-dotted outline-accent-500 -outline-offset-1" : "",
+        "mr-2"
+    ];
+
+});
+
+function formatTrackContext(song: Song) {
+    let context = "";
+    if (song.album_artists) {
+        context += formatArtists(song.album_artists);
+    } else if (song.artists) {
+        context += formatArtists(song.artists);
+    } else {
+        context += "Unknown Artist";
+    }
+    context += " - ";
+    context += song.album ? song.album : "Unknown Album";
+    return context;
+}
+
+function formatTrackDuration(song: Song) {
+    if (!song.duration || isNaN(song.duration)) {
+        return "??:??";
+    }
+    return formatDuration(song.duration);
+}
+</script>
--- src/components/playback/PlaylistSong.vue
+++ src/components/playback/PlaylistSong.vue
@@ -5,48 +5,18 @@
         <div class="basis-8 shrink-0 flex justify-center items-center" :class="!compact ? '-translate-x-2' : ''">
             <span v-if="isCurrent" class="material-icons-round text-ls-600 dark:text-ds-300">play_arrow</span>
         </div>
-        <div class="relative grow h-full min-w-0 rounded-sm flex items-center" :class="rowClass">
-            <div v-if="!compact" class="basis-10 h-10 mr-3 shrink-0 flex items-center">
-                <AlbumArt :url="artworkURL" />
-            </div>
-            <div class="grow basis-0 pr-4 text-ellipsis" :class="{ 'overflow-hidden': song }">
-                <span v-if="song" v-text="formatTrackContext(song)" />
-                <div v-else class="-mr-8 bg-black/5 dark:bg-white/5 h-3 rounded-full" />
-            </div>
-            <div class="basis-8 shrink-0 text-right mr-1">
-                <span v-if="song && song.track_number"> {{ formatTrackNumber(song) }}.</span>
-            </div>
-            <div class="grow basis-0 pr-4 overflow-hidden text-ellipsis">
-                <span v-if="song">
-                    {{ formatTitle(song) }}
-                    <span :class="selected ? '' : 'text-ls-400 dark:text-ds-600'"
-                        v-if="song.artists && song.album_artists && !equals(song.artists, song.album_artists)">
-                        ({{ formatArtists(song.artists) }})
-                    </span>
-                </span>
-                <div v-else class="bg-black/5 dark:bg-white/5 h-3 rounded-full" />
-            </div>
-            <div class="basis-16 shrink-0 text-right">
-                <span v-if="song">{{ formatTrackDuration(song) }}</span>
-                <div v-else class="bg-black/5 dark:bg-white/5 h-3 rounded-full" />
-            </div>
-        </div>
+        <SongListRow :path="entry.path" :compact="compact" :index="index" :selected="selected" :focused="focused"
+            :is-current="isCurrent" />
     </div>
 </template>
 
 <script setup lang="ts">
-import equals from "array-equal"
 import { computed } from 'vue';
 
-import { Song } from '@/api/dto';
-import AlbumArt from '@/components/AlbumArt.vue';
-import { formatArtists, formatDuration, formatTitle, formatTrackNumber } from '@/format';
-import { makeThumbnailURL } from '@/api/endpoints';
 import { PlaylistEntry, usePlaybackStore } from "@/stores/playback";
-import { useSongsStore } from "@/stores/songs";
+import SongListRow from '../SongListRow.vue';
 
 const playback = usePlaybackStore();
-const songs = useSongsStore();
 
 const props = defineProps<{
     entry: PlaylistEntry,
@@ -57,69 +27,5 @@ const props = defineProps<{
     compact: boolean,
 }>();
 
-const song = computed(() => songs.cache.get(props.entry.path));
-
 const isCurrent = computed(() => props.entry.key == playback.currentTrack?.key);
-
-const artworkURL = computed(() => song.value?.artwork ? makeThumbnailURL(song.value.artwork, "tiny") : undefined);
-
-const rowClass = computed(() => {
-
-    const isOdd = props.index % 2 == 1;
-
-    let background;
-    if (props.selected) {
-        background = "bg-accent-100 dark:bg-accent-900";
-    } else if (props.compact) {
-        background = isOdd ? "bg-ls-50 hover:bg-ls-100 dark:bg-ds-800/20 dark:hover:bg-ds-800" : "bg-ls-0 hover:bg-ls-100 dark:bg-ds-900 dark:hover:bg-ds-800";
-    } else {
-        background = [
-            "bg-gradient-to-r from-ls-0 dark:from-ds-900 to-[50px] hover:to-ls-100 dark:hover:to-ds-800",
-            isOdd ? "to-ls-50 dark:to-ds-800/20" : "to-ls-0 dark:to-ds-900",
-        ];
-    }
-
-    let text;
-    if (isCurrent.value && props.selected) {
-        text = "text-accent-700 dark:text-accent-100";
-    } else if (props.selected) {
-        text = "text-accent-700 dark:text-accent-200";
-    } else if (isCurrent.value) {
-        text = "text-ls-700 dark:text-ds-0";
-    } else {
-        text = "text-ls-700 dark:text-ds-400";
-    }
-
-    return [
-        text,
-        background,
-        isCurrent.value ? "font-semibold" : "",
-        props.compact ? "px-3" : "pr-2",
-        !props.compact && props.selected ? "-ml-2 pl-2" : "",
-        props.focused ? "outline-1 outline-dotted outline-accent-500 -outline-offset-1" : "",
-        "mr-2"
-    ];
-
-});
-
-function formatTrackContext(song: Song) {
-    let context = "";
-    if (song.album_artists) {
-        context += formatArtists(song.album_artists);
-    } else if (song.artists) {
-        context += formatArtists(song.artists);
-    } else {
-        context += "Unknown Artist";
-    }
-    context += " - ";
-    context += song.album ? song.album : "Unknown Album";
-    return context;
-}
-
-function formatTrackDuration(song: Song) {
-    if (!song.duration || isNaN(song.duration)) {
-        return "??:??";
-    }
-    return formatDuration(song.duration);
-}
 </script>
