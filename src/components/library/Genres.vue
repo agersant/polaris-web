3de0f339c932bfc629db2cc015616505868e7903
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-27 21:51:04 -0700

    Genres as a tree

--- src/components/library/Genres.vue
+++ src/components/library/Genres.vue
@@ -1,26 +1,53 @@
 <template>
     <div class="flex flex-col whitespace-nowrap">
         <PageTitle label="Genres" />
-        <div class="flex flex-wrap gap-2">
-            <Badge v-for="genre of genres" :label="genre.name" :auto-color="true" />
+
+        <div v-show="treeModel.length" class="grow min-h-0 flex flex-col">
+            <VirtualTree ref="tree" v-model="treeModel" class="grow" />
         </div>
+
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, Ref, shallowRef, watch } from 'vue';
 import { useAsyncState } from '@vueuse/core';
 
+import { GenreHeader } from '@/api/dto';
 import { getGenres } from '@/api/endpoints';
-import Badge from '@/components/basic/Badge.vue';
 import PageTitle from '@/components/basic/PageTitle.vue';
+import VirtualTree from '@/components/basic/VirtualTree.vue';
+import { Node } from "@/components/basic/VirtualTree.vue";
 
 defineProps<{
     genre?: string,
 }>();
 
 const { state: genres, isLoading, error } = useAsyncState(
-    () => getGenres(),
+    () => getGenres().then(g => makeGenreNodes(g)),
     undefined,
 );
+
+const treeModel: Ref<Node[]> = shallowRef([]);
+
+watch(genres, (v) => {
+    if (v && !treeModel.value.length) {
+        treeModel.value = v;
+    }
+});
+
+function makeGenreNodes(genres: GenreHeader[]): Node[] {
+    return genres.map((g, index) => {
+        return {
+            depth: 0,
+            key: g.name,
+            label: g.name,
+            icon: "label",
+            leaf: false,
+        };
+    });
+}
+
+const filterQuery = ref("");
 
 </script>
