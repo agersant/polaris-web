c282f134df1015716468679291acfbe36fe4d9cc
Antoine Gersant <antoine.gersant@lesforges.org>
2024-09-28 20:28:07 -0700

    Color adjustment to support varying backgrounds

--- src/components/AlbumArt.vue
+++ src/components/AlbumArt.vue
@@ -1,14 +1,14 @@
 <template>
     <UseImage :src="url || ''" class="w-full h-full object-cover" :class="rounding">
         <template #loading>
-            <div class="bg-ls-200 text-ls-400 dark:bg-ds-800 dark:text-ds-600 w-full h-full aspect-square flex items-center justify-center"
+            <div class="bg-ls-200 text-ls-400 dark:bg-ds-950/30 dark:text-ds-600 w-full h-full aspect-square flex items-center justify-center"
                 :class="rounding">
                 <Spinner />
             </div>
         </template>
 
         <template #error>
-            <div class="bg-ls-200 dark:bg-ds-800 w-full h-full aspect-square flex items-center justify-center"
+            <div class="bg-ls-200 dark:bg-ds-950/30 w-full h-full aspect-square flex items-center justify-center"
                 :class="rounding">
                 <span class="material-icons-round text-ls-400 dark:text-ds-700">image_not_supported</span>
             </div>
