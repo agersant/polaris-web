de1f2ccaccc10e98bc9a77f59cea75f170ad7f81
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-03 22:07:39 -0700

    Update list of playlists after saving one

--- src/stores/playlists.ts
+++ src/stores/playlists.ts
@@ -1,20 +1,34 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
+import { Ref, ref } from "vue";
+
 import { ListPlaylistsEntry } from "@/api/dto";
-import { playlists } from "@/api/endpoints";
+import { playlists, putPlaylist } from "@/api/endpoints";
+import { usePlaybackStore } from "./playback";
 
 export type PlaylistsState = {
 	listing: ListPlaylistsEntry[];
 };
 
-export const usePlaylistsStore = defineStore("playlists", {
-	state: (): PlaylistsState => ({
-		listing: [],
-	}),
-	actions: {
-		async refresh() {
-			this.listing = await playlists();
-		},
-	},
+export const usePlaylistsStore = defineStore("playlists", () => {
+
+	const listing: Ref<ListPlaylistsEntry[]> = ref([]);
+
+	async function refresh() {
+		listing.value = await playlists();
+	}
+
+	async function save() {
+		const playback = usePlaybackStore();
+		await putPlaylist(playback.name, playback.playlist.map(e => e.path));
+		await refresh();
+	}
+
+	return {
+		listing,
+
+		refresh,
+		save,
+	};
 });
 
 if (import.meta.hot) {
