f9e39d7d121efda845a525792c32759a5a781797
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-09 12:04:32 -0700

    More contrast in dark mode tabs

--- src/components/basic/Tabs.vue
+++ src/components/basic/Tabs.vue
@@ -3,7 +3,7 @@
         <div v-for="tab in tabs" @click="current = tab.key"
             class="cursor-pointer rounded-md px-3 py-2 text-sm font-medium" :class="tab.key == current
                 ? 'bg-accent-100 text-accent-700 dark:bg-ds-700 dark:text-ds-50'
-                : 'text-ls-500 hover:text-ls-700 dark:text-ds-500 dark:hover:text-ls-300'
+                : 'text-ls-500 hover:text-ls-700 dark:text-ds-300 dark:hover:text-ls-100'
                 ">
             {{ tab.label }}
         </div>
