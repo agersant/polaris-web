39287cc39ba43c4ab76d6c919680b736339f8c71
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-09 16:03:14 -0700

    Collection settings mockup

--- src/components/settings/Collection.vue
+++ src/components/settings/Collection.vue
@@ -1,73 +1,62 @@
 <template>
-	<form v-if="settings && mountDirs" v-on:submit.prevent>
-		<div class="field">
-			<label for="art_pattern">Album art pattern</label>
-			<input type="text" id="art_pattern" v-model="settings.album_art_pattern"
-				v-on:change="onAlbumArtPatternChanged" v-on:keypress.enter.prevent placeholder="Folder.(jpg|png)"
-				data-cy="album-art-pattern" />
-			<p class="tip">The regular expression used to detect album art files.</p>
-			<p v-if="!validateAlbumArtPattern()" class="tip error">Please enter a valid regular expression.</p>
+	<div class="flex flex-col gap-8">
+
+
+		<div class="flex flex-col rounded-md p-8 border bg-ls-0 border-ls-200 dark:bg-ds-900 dark:border-ds-700">
+			<SectionTitle label="Music Sources" class="!mb-4" />
+			<div class="flex flex-col gap-4">
+				<div class="flex gap-4">
+					<InputText icon="library_music" placeholder="My Music" />
+					<InputText icon="folder" placeholder="/home/music" />
+				</div>
+				<Button label="Add Source" icon="add" severity="tertiary" class="self-start" />
+			</div>
 		</div>
-		<div class="field sources">
-			<label>Music sources</label>
-			<table>
-				<thead>
-					<th>Location</th>
-					<th>Name</th>
-					<th />
-				</thead>
-				<tr v-for="(mountDir, index) in mountDirs.listing" v-bind:key="index">
-					<td>
-						<input type="text" v-model="mountDir.source" v-on:change="mountDirs.save"
-							v-on:keypress.enter.prevent data-cy="mount-dir-source" />
-					</td>
-					<td>
-						<input type="text" v-model="mountDir.name" v-on:change="mountDirs.save"
-							data-cy="mount-dir-name" />
-					</td>
-					<td>
-						<i v-on:click="mountDirs.remove(mountDir)" class="noselect material-icons md-18"
-							data-cy="delete-mount-dir">delete</i>
-					</td>
-				</tr>
-			</table>
-			<button v-on:click="addMountDir" data-cy="add-mount-dir">Add more</button>
+		<div class="flex flex-col rounded-md p-8 border bg-ls-0 border-ls-200 dark:bg-ds-900 dark:border-ds-700">
+			<SectionTitle label="Album Art Pattern" class="!mb-4" />
+			<InputText id="album-art" icon="image_search" class="w-80" placeholder="Folder.(jpg|png)" />
 		</div>
-		<div class="field sleep_duration">
-			<label for="sleep_duration">
-				Scan collection every
-				<input type="text" id="sleep_duration" v-model="reindexPeriod" @change="onReindexPeriodChanged"
-					data-cy="sleep-duration" />
-				minutes
-			</label>
-			<StateButton v-bind:submit="false" v-bind:states="reindexStates" v-bind:state="reindexState"
-				@click="reindex" data-cy="reindex" />
+		<div class="flex flex-col rounded-md p-8 border bg-ls-0 border-ls-200 dark:bg-ds-900 dark:border-ds-700">
+			<SectionTitle label="Indexing Status" class="!mb-4" />
+			<div class="flex justify-between items-center">
+				<div class="flex flex-col gap-3">
+					<div class="flex items-center gap-2">
+						<div class="flex-none rounded-full bg-green-400/10 p-1.5">
+							<div class="h-3 w-3 rounded-full bg-green-400" />
+						</div>
+						<span class="font-medium text-ls-700 dark:text-ds-200">Up to date</span>
+					</div>
+					<div class="flex items-center gap-2 text-ls-500 dark:text-ds-400">
+						<span class="material-icons-round" v-text="'access_time'" />
+						<span>Scanned 2 minutes ago</span>
+					</div>
+					<div class="flex items-center gap-2 text-ls-500 dark:text-ds-400">
+						<span class="material-icons-round" v-text="'audiotrack'" />
+						<span>43900 songs</span>
+					</div>
+				</div>
+				<Button label="Scan Collection" icon="sync" size="xl" @click="triggerIndex" />
+			</div>
 		</div>
-	</form>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, Ref, ref } from "vue";
+
 import { Settings } from "@/api/dto";
 import { getSettings, putSettings, triggerIndex } from "@/api/endpoints";
 import { useMountDirsStore } from "@/stores/mount-dirs";
-import StateButton, { State } from "@/components/StateButton.vue";
+import Button from "@/components/basic/Button.vue";
+import InputText from "@/components/basic/InputText.vue";
+import SectionTitle from "@/components/basic/SectionTitle.vue";
 
 const mountDirs = useMountDirsStore();
 
 const settings: Ref<Settings | null> = ref(null);
-const reindexPeriod = ref(0);
-const reindexStates: Ref<Record<string, State>> = ref({
-	ready: { name: "Scan now" },
-	applying: { name: "Hold on…", disabled: true },
-	success: { name: "On it!", disabled: true, success: true },
-	failure: { name: "Error :(", disabled: true, failure: true },
-});
-const reindexState: Ref<State> = ref(reindexStates.value.ready);
 
 onMounted(async () => {
 	settings.value = await getSettings();
-	reindexPeriod.value = Math.round(settings.value.reindex_every_n_seconds / 60);
 	mountDirs.refresh();
 });
 
@@ -88,34 +77,8 @@ function validateAlbumArtPattern() {
 	}
 }
 
-function onReindexPeriodChanged() {
-	if (!settings.value) {
-		return;
-	}
-	let periodInSeconds = Math.round(reindexPeriod.value) * 60;
-	if (isNaN(periodInSeconds) || periodInSeconds < 0) {
-		return;
-	}
-	settings.value.reindex_every_n_seconds = periodInSeconds;
-	commitSettings();
-}
-
 function addMountDir() {
 	mountDirs.create();
-}
-
-async function reindex() {
-	reindexState.value = reindexStates.value.applying;
-	const response = await triggerIndex();
-	if (response.status != 200) {
-		reindexState.value = reindexStates.value.failure;
-		console.log("Index trigger error: " + response.status);
-	} else {
-		reindexState.value = reindexStates.value.success;
-	}
-	setTimeout(() => {
-		reindexState.value = reindexStates.value.ready;
-	}, 2000);
 }
 
 function commitSettings() {
@@ -125,14 +88,3 @@ function commitSettings() {
 	putSettings(settings.value);
 }
 </script>
-
-<style scoped>
-.sleep_duration label {
-	display: inline;
-}
-
-.sleep_duration input {
-	width: 50px;
-	text-align: center;
-}
-</style>
