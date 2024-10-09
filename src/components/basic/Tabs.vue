c6c01a0be35dd1ebe5c3a1d8c6d3598df70b4e8d
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-09 12:03:20 -0700

    Settings tabs

---
+++ src/components/basic/Tabs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mb-8 flex space-x-4">
+        <div v-for="tab in tabs" @click="current = tab.key"
+            class="cursor-pointer rounded-md px-3 py-2 text-sm font-medium" :class="tab.key == current
+                ? 'bg-accent-100 text-accent-700 dark:bg-ds-700 dark:text-ds-50'
+                : 'text-ls-500 hover:text-ls-700 dark:text-ds-500 dark:hover:text-ls-300'
+                ">
+            {{ tab.label }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts" generic="T">
+export type TabItem<T> = {
+    key: T,
+    label: string,
+};
+
+defineProps<{ tabs: TabItem<T>[] }>();
+
+const current = defineModel<T>({ required: true });
+</script>
