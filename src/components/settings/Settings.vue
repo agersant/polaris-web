34d5dba9a02bf00f952738d415227fdea1612ddb
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-09 12:03:20 -0700

    Settings tabs

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,23 +1,68 @@
 <template>
-	<div class="pane">
-		<div class="paneHeader">
-			<h2>Settings</h2>
-			<Tabs />
-		</div>
-		<div class="paneContent">
-			<router-view></router-view>
+	<div class="flex flex-col">
+		<PageTitle label="Settings" />
+
+		<Tabs v-model="category" :tabs="tabs" />
+
+		<div class="grow min-h-0 flex flex-col">
+			<router-view />
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import Tabs from "./Tabs.vue";
-</script>
+import { computed, ComputedRef } from 'vue';
+import { useRouter } from 'vue-router';
 
-<style scoped>
-.paneHeader {
-	padding-bottom: 0px;
-	justify-content: space-between;
-	overflow: visible;
-}
-</style>
+import PageTitle from '@/components/basic/PageTitle.vue';
+import Tabs, { TabItem } from '@/components/basic/Tabs.vue';
+import { useUserStore } from '@/stores/user';
+
+type Category = "preferences" | "collection" | "users" | "ddns";
+
+const router = useRouter();
+const user = useUserStore();
+
+const tabs = computed(() => {
+	let items: TabItem<Category>[] = [];
+	items.push({ label: "Preferences", key: "preferences" });
+	if (user.isAdmin) {
+		items.push({ label: "Collection", key: "collection" });
+		items.push({ label: "Users", key: "users" });
+		items.push({ label: "Dynamic DNS", key: "ddns" });
+	}
+	return items;
+});
+
+const category: ComputedRef<Category> = computed({
+	get: () => {
+		const path = router.currentRoute.value.path;
+		if (path.endsWith("/collection")) {
+			return "collection"
+		} else if (path.endsWith("/users")) {
+			return "users"
+		} else if (path.endsWith("/ddns")) {
+			return "ddns"
+		} else {
+			return "preferences";
+		}
+	},
+	set: (value) => {
+		switch (value) {
+			case "preferences":
+				router.push("/settings/preferences");
+				break;
+			case "collection":
+				router.push("/settings/collection");
+				break;
+			case "users":
+				router.push("/settings/users");
+				break;
+			case "ddns":
+				router.push("/settings/ddns");
+				break;
+		}
+	},
+});
+
+</script>
