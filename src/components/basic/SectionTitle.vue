a792899c415c1d80fe356da293bafaf4eb96f039
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-12 21:57:11 -0700

    Adds left slot

--- src/components/basic/SectionTitle.vue
+++ src/components/basic/SectionTitle.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="mb-8 flex items-center gap-4 justify-between">
         <div class="flex items-center gap-2">
+            <slot name="left" />
             <span v-if="icon" class="material-icons-round text-ls-400 dark:text-ds-500" v-text="icon" />
             <div v-text="label" class="text-sm uppercase font-medium text-ls-500 dark:text-ds-400" />
             <slot />
