8c71f7760e68662c9c37d8f88771ac705d64921e
Antoine Gersant <antoine.gersant@lesforges.org>
2024-10-13 17:45:09 -0700

    Adds optional label and min/max values

--- src/components/basic/Slider.vue
+++ src/components/basic/Slider.vue
@@ -1,14 +1,21 @@
 <template>
-    <div ref="root" @click="snapToCursor" @keydown="onKeyDown" tabindex="-1"
-        class="cursor-pointer group relative h-1.5 rounded-full bg-ls-300 dark:bg-ds-700">
-        <div class="absolute h-full rounded-full bg-accent-600 dark:bg-accent-700" :style="`width: ${100 * model}%`" />
-        <div class="cursor-grab
-        absolute top-1/2 -translate-x-1/2 -translate-y-1/2 w-5 h-5
-        rounded-full shadow-sm border-2
-        bg-ls-0 dark:bg-ds-900 
-        border-ls-300 dark:border-ds-700
-        group-active:border-accent-500 group-focus:border-accent-500
-        dark:group-active:border-accent-600 dark:group-focus:border-accent-600" :style="`left: ${100 * model}%`" />
+    <div>
+        <div v-if="label" class="block mb-2 text-sm font-medium leading-6 text-ls-900 dark:text-ds-0">
+            {{ label }}
+        </div>
+        <div ref="root" @click="snapToCursor" @keydown="onKeyDown" tabindex="-1"
+            class="cursor-pointer group relative h-1.5 rounded-full bg-ls-300 dark:bg-ds-700">
+            <div class="absolute h-full rounded-full bg-accent-600 dark:bg-accent-700"
+                :style="`width: ${100 * unscale(model)}%`" />
+            <div class="cursor-grab
+            absolute top-1/2 -translate-x-1/2 -translate-y-1/2 w-5 h-5
+            rounded-full shadow-sm border-2
+            bg-ls-0 dark:bg-ds-900 
+            border-ls-300 dark:border-ds-700
+            group-active:border-accent-500 group-focus:border-accent-500
+            dark:group-active:border-accent-600 dark:group-focus:border-accent-600"
+                :style="`left: ${100 * unscale(model)}%`" />
+        </div>
     </div>
 </template>
 
@@ -17,6 +24,12 @@ import { onMounted, useTemplateRef, watch, } from 'vue';
 import { useMouseInElement, useMousePressed, watchPausable } from '@vueuse/core';
 
 const model = defineModel<number>({ required: true });
+
+const { min = 0, max = 1 } = defineProps<{
+    min?: number,
+    max?: number
+    label?: string,
+}>();
 
 const root = useTemplateRef("root");
 
@@ -34,19 +47,27 @@ watch(pressed, (down) => {
     }
 });
 
+function scale(value: number) {
+    return min + (max - min) * Math.max(0, Math.min(value, 1));
+}
+
+function unscale(value: number) {
+    return (value - min) / (max - min);
+}
+
 function onKeyDown(event: KeyboardEvent) {
     switch (event.code) {
         case 'ArrowLeft':
-            model.value = Math.max(0, Math.min(model.value - 0.02, 1));
+            model.value = scale(unscale(model.value) - 0.02);
             break;
         case 'ArrowRight':
-            model.value = Math.max(0, Math.min(model.value + 0.02, 1));
+            model.value = scale(unscale(model.value) + 0.02);
             break;
     }
 }
 
 function snapToCursor() {
-    model.value = Math.max(0, Math.min(mouseX.value / width.value, 1));
+    model.value = scale(mouseX.value / width.value);
     root.value?.focus();
 }
 </script>
