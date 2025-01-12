37c3151241683d0fd2eb98c52947965688a5793e
Antoine Gersant <antoine.gersant@lesforges.org>
2025-01-11 18:36:52 -0800

    Fixed z-fighting

--- src/components/basic/SidePanelSetup.vue
+++ src/components/basic/SidePanelSetup.vue
@@ -2,11 +2,11 @@
     <div class="absolute h-full w-full pointer-events-none">
         <div class="relative h-full w-full">
             <ScreenFade>
-                <ScreenDarkening v-show="open" @click="close" />
+                <ScreenDarkening v-show="open" @click="close" class="z-10" />
             </ScreenFade>
             <Transition appear name="slide">
                 <div v-show="open"
-                    class="pointer-events-auto absolute right-0 h-full flex flex-col w-screen max-w-2xl bg-ls-0 dark:bg-ds-900 p-12 shadow-xl">
+                    class="z-20 pointer-events-auto absolute right-0 h-full flex flex-col w-screen max-w-2xl bg-ls-0 dark:bg-ds-900 p-12 shadow-xl">
                     <div class="absolute right-12 top-11">
                         <Button icon="close" severity="tertiary" @click="close" />
                     </div>
