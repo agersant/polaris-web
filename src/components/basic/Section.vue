654eeb1dbed63c732267b8f6166ab0d1df3d462b
Antoine Gersant <antoine.gersant@lesforges.org>
2026-02-01 18:03:19 -0800

    Removed section outlines in dark mode

--- src/components/basic/Section.vue
+++ src/components/basic/Section.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rounded-md p-8 border bg-ls-0 border-ls-200 dark:bg-ds-900 dark:border-ds-700">
+    <div class="rounded-md p-8 bg-ls-0 border border-ls-200 dark:border-0 dark:bg-ds-900 dark:border-ds-700">
         <slot />
     </div>
 </template>
