ca3e6c34b0d74727d0c95dceabfbd77b8112c0fe
Antoine Gersant <antoine.gersant@lesforges.org>
2026-02-06 23:21:11 -0800

    Removed blur from screen overlays

--- src/components/basic/ScreenDarkening.vue
+++ src/components/basic/ScreenDarkening.vue
@@ -1,4 +1,3 @@
 <template>
-    <div
-        class="pointer-events-auto fixed backdrop-brightness-50 backdrop-grayscale backdrop-blur-sm left-0 top-0 w-screen h-screen" />
+    <div class="pointer-events-auto fixed backdrop-brightness-50 backdrop-grayscale left-0 top-0 w-screen h-screen" />
 </template>
