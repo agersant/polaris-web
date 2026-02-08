48f5a414fc3ea308841437d263651aab18d8016b
Antoine Gersant <antoine.gersant@lesforges.org>
2026-02-08 00:42:00 -0800

    Context menu spacing improvements

--- src/components/basic/ContextMenu.vue
+++ src/components/basic/ContextMenu.vue
@@ -7,12 +7,12 @@
 				border border-ls-200 dark:border-ds-700
 				shadow-lg shadow-accent-800/20 dark:shadow-accent-900/20">
 				<div v-for="item in items"
-					class="group cursor-pointer flex justify-between gap-12 px-3 py-1.5 rounded-sm hover:bg-accent-100 dark:hover:bg-accent-900"
+					class="group cursor-pointer flex justify-between gap-8 px-3 py-1.5 rounded-sm hover:bg-accent-100 dark:hover:bg-accent-900"
 					@click="execute(item)">
 					<div v-text="item.label"
 						class="font-medium text-ls-700 dark:text-ds-300 group-hover:text-accent-700 dark:group-hover:text-accent-200" />
-					<div v-if="item.shortcut" v-text="item.shortcut"
-						class="text-ls-400 dark:text-ds-600 group-hover:text-accent-400 dark:group-hover:text-accent-400" />
+					<div v-if="items.find(s => s.shortcut)" v-text="item.shortcut"
+						class="text-right min-w-20 text-ls-400 dark:text-ds-600 group-hover:text-accent-400 dark:group-hover:text-accent-400" />
 				</div>
 			</div>
 		</FloatingWidget>
